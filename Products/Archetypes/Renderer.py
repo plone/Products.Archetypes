cb0101b366c02b672e9b6704ebc2efbb94102349
Lalo Martins <lalo@laranja.org>
2003-03-20 18:47:45 +0000

    disabling exception catching where it shouldn't happen
    svn path=/Archetypes/trunk/; revision=533

--- Products/Archetypes/Renderer.py
+++ Products/Archetypes/Renderer.py
@@ -8,20 +8,6 @@ _marker = []
 class ArchetypesRenderer(base):
     __implements__ = (ILayer,)
 
-    # broken - capturing the exception at this level is worse than not doing it
-    # (as it will return something invalid and cause a very hard to debug error)
-    # if you disagree, talk to lalo ;-)
-##     def render(self, field_name, mode, widget, instance, **kwargs):
-##         try:
-##             value = base.render(self, field_name, mode, widget,
-##                                 instance, **kwargs)
-
-##         except Exception, E:
-##             log_exc()
-##             value = "%s: %s" % (str(type(E)), E)
-
-##         return value
-    
     def setupContext(self, field_name, mode, widget, instance,
                      **kwargs):
 
