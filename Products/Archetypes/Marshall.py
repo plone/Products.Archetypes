37fd68f56689c6edaa4a8f237acc1838af6676b9
Sidnei da Silva <sidnei.da.silva@gmail.com>
2003-04-12 14:48:51 +0000

    Fixing a broken import
    svn path=/Archetypes/trunk/; revision=626

--- Products/Archetypes/Marshall.py
+++ Products/Archetypes/Marshall.py
@@ -1,38 +1,18 @@
-from Layer import ILayer
-#from Products.CMFCore.utils import getToolByName
-
-class IMarshall(ILayer):
-    """De/Marshall data.
-    demarshall returns a dict to the calling method so that it can
-    place the values into the object as needed
-
-    Marshall knows about the schema on instance and can directly pull
-    the value and return a data object.
-    """
-    
-    def demarshall(instance, data, **kwargs):
-        """given the blob 'data' demarshall it into a dict of k/v
-        pairs"""
-        
-
-    def marshall(instance, **kwargs):
-        """returns a tuple of content-type, length, and data"""
-        
-        
-
+from interfaces.marshall import IMarshall
+from interfaces.layer import ILayer
 
 class Marshaller:
-    __implements__ = (IMarshall, ILayer)    
+    __implements__ = (IMarshall, ILayer)
 
     def __init__(self, demarshall_hook=None, marshall_hook=None):
         self.demarshall_hook = demarshall_hook
         self.marshall_hook = marshall_hook
-        
+
     def initializeInstance(self, instance):
         self.instance.demarshall_hook = getattr(instance, self.demarshall_hook)
         self.instance.marshall_hook = getattr(instance, self.marshall_hook)
-        
-    
+
+
 class DublinCoreMarshaller(Marshaller):
     ## XXX TODO -- based on CMFCore.Document
     def marshall(self, instance, **kwargs):
@@ -46,7 +26,7 @@ class PrimaryFieldMarshaller(Marshaller):
     def demarshall(self, instance, data, **kwargs):
         p = instance.getPrimaryField()
         p.set(instance, data, **kwargs)
-        
+
 
     def marshall(self, instance, **kwargs):
         p = instance.getPrimaryField()
@@ -68,7 +48,7 @@ class PrimaryFieldMarshaller(Marshaller):
 
         if length is None:
             return None
-        
+
         return (content_type, length, data)
-        
-        
+
+
