dab0c282b102b58d77baa8265e43030d66124532
syt <syt@localhost>
2003-07-03 14:21:30 +0000

    correctly remove previous language from session variables
    svn path=/Archetypes/trunk/; revision=829

--- Products/Archetypes/skins/archetypes/change_lang.py
+++ Products/Archetypes/skins/archetypes/change_lang.py
@@ -12,7 +12,6 @@ if context.hasI18NContent():
    oldLang = REQUEST.SESSION.get('PREV_CONTENT_LANGUAGE', None)
    if oldLang is not None:
       context.setCurrentLanguage(lang=oldLang)
-   else:
       del REQUEST.SESSION['PREV_CONTENT_LANGUAGE']
       
 portal_status_message = REQUEST.get('portal_status_message', 'Content changes saved.')
