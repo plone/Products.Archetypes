1197fef3fc2da61b036fd3985eacaa9c63dc73ca
syt <syt@localhost>
2003-07-03 09:43:29 +0000

    some scripts / template to handle the current language selection
    svn path=/Archetypes/trunk/; revision=814

---
+++ Products/Archetypes/skins/i18n_content_scripts/getContentLanguage.py
@@ -0,0 +1,23 @@
+## Script (Python) "getContentLanguage"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##title=return the id for the current content language (context sensitive)
+##parameters=lang=None
+
+if not lang and hasattr(context, 'hasI18NContent') and context.hasI18NContent():
+    lang = context.getCurrentLanguage()
+
+if not lang and hasattr(context, 'Language'):
+    lang = context.Language()
+    
+if not lang:
+    try:
+        sp = context.portal_properties.site_properties
+        lang = sp.getProperty('default_language', 'en')
+    except AttributeError:
+        lang = None
+
+return lang or "en"
