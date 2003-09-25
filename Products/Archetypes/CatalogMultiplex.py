cd29212a91d1c66f124605b18f21cca5ca96f0c5
Benjamin Saller <bcsaller@objectrealms.net>
2003-09-25 18:45:06 +0000

    better, faster, leaner cataloging YSFM
    svn path=/Archetypes/trunk/; revision=1062

--- Products/Archetypes/CatalogMultiplex.py
+++ Products/Archetypes/CatalogMultiplex.py
@@ -30,27 +30,12 @@ class CatalogMultiplex(CMFCatalogAware):
     def reindexObject(self, idxs=[]):
         at = getToolByName(self, TOOL_NAME)
         catalogs = at.getCatalogsByType(self.meta_type)
+
+        if idxs == []:
+            if hasattr(aq_base(self), 'notifyModified'):
+                self.notifyModified()
+
         for c in catalogs:
-            if idxs == []:
-                if hasattr(aq_base(self), 'notifyModified'):
-                    self.notifyModified()
             if c is not None:
                 c.catalog_object(self, self.__url(), idxs=idxs)
             
-    security.declareProtected(ModifyPortalContent, 'reindexObjectSecurity')
-    def reindexObjectSecurity(self):
-        at = getToolByName(self, TOOL_NAME)
-        catalogs = at.getCatalogsByType(self.meta_type)
-        for c in catalogs:
-            path = '/'.join(self.getPhysicalPath())
-            for brain in c.searchResults(path=path):
-                ob = brain.getObject()
-                if ob is None:
-                    # Ignore old references to deleted objects.
-                    continue
-                s = getattr(ob, '_p_changed', 0)
-                self.reindexObject(idxs=['allowedRolesAndUsers'])
-                if s is None: ob._p_deactivate()
-
-            c.catalog_object(self, self.__url(), (['allowedRolesAndUsers']))
-
