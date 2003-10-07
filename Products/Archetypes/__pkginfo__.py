806e73c3cc710a2cd056e55cb6368c9543c3ed8d
syt <syt@localhost>
2003-10-07 10:56:41 +0000

    update debian files for zope 2.6.2 and python 2.2
    svn path=/Archetypes/trunk/; revision=1080

--- Products/Archetypes/__pkginfo__.py
+++ Products/Archetypes/__pkginfo__.py
@@ -34,3 +34,8 @@ Features
 web = "http://www.sourceforge.net/projects/archetypes"
 ftp = ""
 mailing_list = "archetypes-devel@lists.sourceforge.net"
+
+debian_name = "zope-cmfarchetypes"
+debian_maintainer = "Sylvain Thenault"
+debian_maintainer_email = "sylvain.thenault@logilab.fr"
+debian_handler = "zope"
