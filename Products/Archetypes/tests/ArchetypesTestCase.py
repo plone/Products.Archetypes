811dc279d3ba96bdff77c251701698b3a75187ef
Christian 'Tiran' Heimes <lists@cheimes.de>
2003-10-20 17:34:25 +0000

    fixed comment and imported Products
    svn path=/Archetypes/branches/tiran-zopetestcase-branch/; revision=1104

--- Products/Archetypes/tests/ArchetypesTestCase.py
+++ Products/Archetypes/tests/ArchetypesTestCase.py
@@ -1,8 +1,8 @@
 #
-# PloneTestCase
+# ArchetypesTestCase
 #
 
-# $Id: ArchetypesTestCase.py,v 1.1.2.1 2003/10/20 17:09:17 tiran Exp $
+# $Id: ArchetypesTestCase.py,v 1.1.2.2 2003/10/20 17:34:25 tiran Exp $
 
 from Testing import ZopeTestCase
 
@@ -11,6 +11,12 @@ ZopeTestCase.installProduct('CMFDefault')
 ZopeTestCase.installProduct('CMFCalendar')
 ZopeTestCase.installProduct('CMFTopic')
 ZopeTestCase.installProduct('DCWorkflow')
+ZopeTestCase.installProduct('CMFActionIcons')
+ZopeTestCase.installProduct('CMFQuickInstallerTool')
+ZopeTestCase.installProduct('CMFFormController')
+ZopeTestCase.installProduct('GroupUserFolder')
+ZopeTestCase.installProduct('ZCTextIndex')
+ZopeTestCase.installProduct('CMFPlone')
 ZopeTestCase.installProduct('MailHost', quiet=1)
 ZopeTestCase.installProduct('PageTemplates', quiet=1)
 ZopeTestCase.installProduct('PythonScripts', quiet=1)
