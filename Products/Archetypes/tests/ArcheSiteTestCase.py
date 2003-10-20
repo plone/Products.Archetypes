7ca5a9b805b3ca91e43701f90c21317f4ef4dd77
Christian 'Tiran' Heimes <lists@cheimes.de>
2003-10-20 17:32:31 +0000

    added archetypes site test case
    svn path=/Archetypes/branches/tiran-zopetestcase-branch/; revision=1103

---
+++ Products/Archetypes/tests/ArcheSiteTestCase.py
@@ -0,0 +1,13 @@
+#
+# ArcheSiteTestCase
+#
+
+# $Id: ArcheSiteTestCase.py,v 1.1.2.1 2003/10/20 17:32:31 tiran Exp $
+
+from Testing import ZopeTestCase
+
+class ArcheSiteTestCase(ZopeTestCase.ZopeTestCase):
+    """ archetypes test case with plone site """
+    # XXX TODO
+    
+    pass    
