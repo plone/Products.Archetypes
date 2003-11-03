725287f37ea1f33ef1075f6f85e3d472a0f42317
Sidnei da Silva <sidnei.da.silva@gmail.com>
2003-11-03 22:05:22 +0000

    Installe ArchetypesTestUpdateSchema
    svn path=/Archetypes/trunk/; revision=1155

--- Products/Archetypes/tests/ArchetypesTestCase.py
+++ Products/Archetypes/tests/ArchetypesTestCase.py
@@ -2,7 +2,7 @@
 # ArchetypesTestCase
 #
 
-# $Id: ArchetypesTestCase.py,v 1.2 2003/11/03 21:44:22 dreamcatcher Exp $
+# $Id: ArchetypesTestCase.py,v 1.3 2003/11/03 22:05:22 dreamcatcher Exp $
 
 from Testing import ZopeTestCase
 
@@ -20,10 +20,11 @@ ZopeTestCase.installProduct('CMFPlone')
 ZopeTestCase.installProduct('MailHost', quiet=1)
 ZopeTestCase.installProduct('PageTemplates', quiet=1)
 ZopeTestCase.installProduct('PythonScripts', quiet=1)
-ZopeTestCase.installProduct('ExternalMethod', quiet=1) 
+ZopeTestCase.installProduct('ExternalMethod', quiet=1)
 ZopeTestCase.installProduct('ZCatalog')
 ZopeTestCase.installProduct('Archetypes')
+ZopeTestCase.installProduct('ArchetypesTestUpdateSchema')
 ZopeTestCase.installProduct('PortalTransforms')
 
 class ArchetypesTestCase(ZopeTestCase.ZopeTestCase):
-    pass    
+    pass
