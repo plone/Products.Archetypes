54cc664b48996383ddf9f513a9a5e0f185e754f7
Benjamin Saller <bcsaller@objectrealms.net>
2003-12-15 08:12:30 +0000

    need to get the tool is we installed it
    svn path=/Archetypes/trunk/; revision=1341

--- Products/Archetypes/Extensions/utils.py
+++ Products/Archetypes/Extensions/utils.py
@@ -39,6 +39,7 @@ def install_tools(self, out):
         addTool = self.manage_addProduct['Archetypes'].manage_addTool
         addTool('Archetype Tool')
 
+        at = getToolByName(self, 'archetype_tool', None)
         ##Test some of the templating code
         at.registerTemplate('base_view', "Normal View")
     else:
