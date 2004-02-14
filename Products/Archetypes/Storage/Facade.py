275b21ffe7e7c2f1a09bce78bac8cb061d9046bb
Sidnei da Silva <sidnei.da.silva@gmail.com>
2004-02-14 03:44:26 +0000

    Make Storage a package. Add Facade implementation for CMFMetadata.
    svn path=/Archetypes/branches/cmfmetadata-integration-branch/; revision=1478

---
+++ Products/Archetypes/Storage/Facade.py
@@ -0,0 +1,56 @@
+from Products.CMFCore.utils import getToolByName
+from Products.Archetypes.Storage import StorageLayer
+from Products.Archetypes.interfaces.storage import IStorage
+from Products.Archetypes.interfaces.layer import ILayer
+from Products.Archetypes.Field import encode, decode
+
+class FacadeMetadataStorage(StorageLayer):
+    """A Facade Storage which delegates to
+    CMFMetadata's Metadata Tool for actually
+    storing the metadata values
+    """
+
+    def __init__(self, metadata_set):
+        self.metadata_set = metadata_set
+
+    __implements__ = (IStorage, ILayer)
+
+    def getTool(self, instance):
+        return getToolByName(instance, 'portal_metadata')
+
+    def initializeInstance(self, instance, item=None, container=None):
+        pass
+
+    def initializeField(self, instance, field):
+        pass
+
+    def get(self, name, instance, **kwargs):
+        field = kwargs['field']
+        tool = self.getTool(instance)
+        mdata = tool.getMetadata(instance)
+        value = mdata[self.metadata_set][name]
+        return value
+
+    def set(self, name, instance, value, **kwargs):
+        field = kwargs['field']
+        tool = self.getTool(instance)
+        mdata = tool.getMetadata(instance)
+        if type(value) == type(u''):
+            value = encode(value, instance)
+        data = {name:value}
+        # Calling _setData directly, because there's
+        # *no* method for setting one field at a time,
+        # and setValues takes a dict and does
+        # validation, which prevents us from setting
+        # values.
+        mdata._setData(data, set_id=self.metadata_set)
+
+    def unset(self, name, instance, **kwargs):
+        pass
+
+    def cleanupField(self, instance, field, **kwargs):
+        pass
+
+    def cleanupInstance(self, instance, item=None, container=None):
+        pass
+
