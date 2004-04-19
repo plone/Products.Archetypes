d6776fcce73da81dc23f7e1759ca337d4da4965e
Kapil Thangavelu <k_vertigo@users.sourceforge.net>
2004-04-19 08:35:48 +0000

    - remove schema check, no longer present  - sync handle_mode invocation with classgen impl
    svn path=/Archetypes/branches/schema_provider-branch/; revision=1750

--- Products/Archetypes/VariableSchemaSupport.py
+++ Products/Archetypes/VariableSchemaSupport.py
@@ -36,8 +36,6 @@ class VarClassGen(ClassGenerator):
         klass.archetype_name = getattr(klass, 'archetype_name',
                                        self.generateName(klass))
 
-        self.checkSchema(klass)
-
         fields = self.schema.fields()
         generator = Generator()
         for field in fields:
@@ -47,9 +45,9 @@ class VarClassGen(ClassGenerator):
             if "c" not in field.generateMode: continue
             type = getattr(klass, 'type')
             for mode in field.mode: #(r, w)
-                self.handle_mode(klass, generator, type, field, mode)
+                self.handle_mode(klass, generator, field, mode, None)
                 if mode == 'w':
-                    self.handle_mode(klass, generator, type, field, 'm')
+                    self.handle_mode(klass, generator, field, 'm', None)
 
         InitializeClass(klass)
 
