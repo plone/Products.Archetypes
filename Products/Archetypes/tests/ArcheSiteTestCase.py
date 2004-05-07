56208dd371fa558e7a72b1d8b3e5f34d2c14060e
Stefan H. Holek <stefan@epy.co.at>
2004-05-07 17:25:16 +0000

    Fixup the ArcheSiteTestCase as it was impossible to run the tests on Mac OS X.
    svn path=/Archetypes/branches/release-1_3-branch/; revision=1815

--- Products/Archetypes/tests/ArcheSiteTestCase.py
+++ Products/Archetypes/tests/ArcheSiteTestCase.py
@@ -2,18 +2,20 @@
 # ArcheSiteTestCase
 #
 
-# $Id: ArcheSiteTestCase.py,v 1.2 2003/11/03 21:44:22 dreamcatcher Exp $
+# $Id: ArcheSiteTestCase.py,v 1.2.24.1 2004/05/07 17:25:16 shh42 Exp $
 
-import time
+from Testing import ZopeTestCase
+from Products.CMFPlone.tests import PloneTestCase
 
 from AccessControl.SecurityManagement import newSecurityManager
 from AccessControl.SecurityManagement import noSecurityManager
 from Acquisition import aq_base
-from Testing import ZopeTestCase
+import time
 
 from Products.Archetypes.Extensions.Install import install as installArchetypes
 
-from Products.CMFPlone.tests import PloneTestCase
+portal_name = PloneTestCase.portal_name
+
 
 class ArcheSiteTestCase(PloneTestCase.PloneTestCase):
     """ archetypes test case with plone site """
@@ -26,25 +28,28 @@ class ArcheSiteTestCase(PloneTestCase.PloneTestCase):
         uf = self.portal.acl_users
         return uf.getUserById('PloneMember').__of__(uf)
 
-def setupArchetypes(app, quiet=0):
-    if not hasattr(app.portal, 'archetype_tool'):
+
+def setupArchetypes(app, id=portal_name, quiet=0):
+    portal = app[id]
+    if not hasattr(aq_base(portal), 'archetype_tool'):
         get_transaction().begin()
         _start = time.time()
         if not quiet: ZopeTestCase._print('Adding Archetypes ... ')
 
-        uf = app.portal.acl_users
+        uf = portal.acl_users
         # setup
-        uf._doAddUser('PloneMember', '', ['Members'], [])
+        uf._doAddUser('PloneMember', '', ['Member'], [])
         uf._doAddUser('PloneManager', '', ['Manager'], [])
         # login as manager
         user = uf.getUserById('PloneManager').__of__(uf)
         newSecurityManager(None, user)
         # add Archetypes
-        installArchetypes(app.portal, include_demo=1)
+        installArchetypes(portal, include_demo=1)
         # Log out
         noSecurityManager()
         get_transaction().commit()
         if not quiet: ZopeTestCase._print('done (%.3fs)\n' % (time.time()-_start,))
+
 
 app = ZopeTestCase.app()
 setupArchetypes(app)
