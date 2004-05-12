22653d361f45b5092e2c534cc8b48d825215c062
Daniel Nouri <dpunktnpunkt@users.sourceforge.net>
2004-05-12 11:55:38 +0000

    Hardcoded ugliness: do not return portal_catalog for "Reference" type.
    svn path=/Archetypes/branches/release-1_3-branch/; revision=1855

--- Products/Archetypes/ArchetypeTool.py
+++ Products/Archetypes/ArchetypeTool.py
@@ -15,7 +15,7 @@ from Products.Archetypes.interfaces.metadata import IExtensibleMetadata
 from Products.Archetypes.ClassGen import generateClass, generateCtor, \
      generateZMICtor
 from Products.Archetypes.SQLStorageConfig import SQLStorageConfig
-from Products.Archetypes.config  import PKG_NAME, TOOL_NAME, UID_CATALOG
+from Products.Archetypes.config import PKG_NAME, TOOL_NAME, UID_CATALOG
 from Products.Archetypes.debug import log, log_exc
 from Products.Archetypes.utils import capitalize, findDict, DisplayList, \
      unique, mapply
@@ -435,6 +435,7 @@ class ArchetypeTool(UniqueObject, ActionProviderBase, \
         self._registeredTemplates = PersistentMapping()
         # meta_type -> [names of CatalogTools]
         self.catalog_map = PersistentMapping()
+        self.catalog_map['Reference'] = [] # References not in portal_catalog
         # DM (avoid persistency bug): "_types" now maps known schemas to signatures
         self._types = {}
 ##        for k, t in _types.items():
