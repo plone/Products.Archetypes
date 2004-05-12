0319c049e5e3768d7ed435f5c3ddef10280bca17
Daniel Nouri <dpunktnpunkt@users.sourceforge.net>
2004-05-12 10:47:14 +0000

    UUID is not an index...
    svn path=/Archetypes/branches/release-1_3-branch/; revision=1854

--- Products/Archetypes/ReferenceEngine.py
+++ Products/Archetypes/ReferenceEngine.py
@@ -433,7 +433,7 @@ class ReferenceCatalog(UniqueObject, BTreeFolder2, ReferenceResolver, ZCatalog):
             uuid = obj
             #and we look up the object
             uid_catalog = getToolByName(self, UID_CATALOG)
-            brains = uid_catalog(UUID=uuid)
+            brains = uid_catalog(UID=uuid)
             obj = brains[0].getObject()
 
         return uuid, obj
