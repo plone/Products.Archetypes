8e12a9a1e35510e174a8ef7c37387ab559b0dae0
clebeaupin <clebeaupin@localhost>
2004-06-30 10:15:00 +0000

    *** empty log message ***
    svn path=/Archetypes/branches/release-1_3-branch/; revision=2007

--- Products/Archetypes/tests/test_baseobject.py
+++ Products/Archetypes/tests/test_baseobject.py
@@ -1,5 +1,4 @@
 # -*- coding: iso-8859-1 -*-
-
 import os, sys
 if __name__ == '__main__':
     execfile(os.path.join(sys.path[0], 'framework.py'))
@@ -15,37 +14,35 @@ from Products.Archetypes.public import *
 from Products.Archetypes.config import PKG_NAME
 from Products.Archetypes.BaseUnit import BaseUnit
 
-from test_classgen import Dummy
 from types import StringType
 
-MULTIPLEFIELD_LIST = DisplayList(
+class Dummy(BaseContent):
+
+    MULTIPLEFIELD_LIST = DisplayList(
     (
     ('1', 'Option 1 : printemps'),
     ('2', 'Option 2 : été'),
     ('3', 'Option 3 : automne'),
     ('4', 'Option 3 : hiver'),
     ))
+    
+    
+    schema = BaseSchema + Schema((
+        LinesField(
+            'MULTIPLEFIELD',
+            searchable = 1,
+            vocabulary = MULTIPLEFIELD_LIST,
+            widget = MultiSelectionWidget(
+                i18n_domain = 'plone',
+                ),
+            ), 
+                ))
 
-
-Dummy.schema = BaseSchema + Schema((
-    LinesField(
-        'MULTIPLEFIELD',
-        searchable = 1,
-        vocabulary = MULTIPLEFIELD_LIST,
-        widget = MultiSelectionWidget(
-            i18n_domain = 'plone',
-            ),
-        ), 
-            ))
-            
-
-def getCharset(self):
-    return 'iso-8859-1'
-
-Dummy.getCharset = getCharset
+    def getCharset(self):
+        return 'iso-8859-1'
 
 class BaseObjectTest( ArchetypesTestCase ):
-
+    
     def test_searchableText(self):
         """
         Fix bug [ 951955 ] BaseObject/SearchableText and list of Unicode stuffs
@@ -54,6 +51,7 @@ class BaseObjectTest( ArchetypesTestCase ):
         registerType(Dummy)
         content_types, constructors, ftis = process_types(listTypes(), PKG_NAME)
         dummy = Dummy(oid='dummy')
+        
         # Put dummy in context of portal
         dummy.initializeArchetype()
         
