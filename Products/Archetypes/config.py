eff26a727fb16c0ff6d12edd2c51404d4e99dc58
Christian 'Tiran' Heimes <lists@cheimes.de>
2004-07-11 14:39:05 +0000

    Don't install demo types (committed by accident)
    svn path=/Archetypes/branches/release-1_3-branch/; revision=2070

--- Products/Archetypes/config.py
+++ Products/Archetypes/config.py
@@ -5,7 +5,7 @@ TOOL_NAME = "archetype_tool" ## Name the tool will be installed under
 UID_CATALOG = "uid_catalog"
 
 REGISTER_DEMO_TYPES = 1 ##Initialize the demo types
-INSTALL_DEMO_TYPES = 1 ##Install the demo types
+INSTALL_DEMO_TYPES = 0 ##Install the demo types
 DEBUG = 1  ## See debug messages
 
 ##Reference Engine bits
