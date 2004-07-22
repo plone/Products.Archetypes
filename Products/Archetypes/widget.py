4effc46995193304e4b6300383cfbf84b72fba3e
Benjamin Saller <bcsaller@objectrealms.net>
2004-07-22 13:26:43 +0000

    * we want to catch both exceptions here, this shouldn't impact anyone, but some people playing with traversal hooks *cough* might return the wrong exception
    svn path=/generator/trunk/; revision=2695

--- Products/Archetypes/widget.py
+++ Products/Archetypes/widget.py
@@ -140,7 +140,7 @@ class macrowidget(widget):
                 template = instance.restrictedTraverse(path = path)
                 if template:
                     return template.macros[mode]
-            except Unauthorized:
+            except (Unauthorized, AttributeError):
                 # This means we didn't have access or it doesn't
                 # exit
                 pass
