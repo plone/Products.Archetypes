518661412fa8dadc33b13a52afd222425ba9f20d
Christian 'Tiran' Heimes <lists@cheimes.de>
2004-09-14 17:37:02 +0000

    Added SQL* and VariableSchemaSupport to public.py. Please import only from public!
    svn path=/Archetypes/branches/release-1_3-branch/; revision=3014

--- Products/Archetypes/public.py
+++ Products/Archetypes/public.py
@@ -1,31 +1,47 @@
+# registering and type processing
 from Products.Archetypes.ArchetypeTool import registerType
 from Products.Archetypes.ArchetypeTool import process_types
 from Products.Archetypes.ArchetypeTool import listTypes
 from Products.Archetypes.ArchetypeTool import registerClasses
+# base classes
 from Products.Archetypes.BaseObject import BaseObject
-from Products.Archetypes.BaseObject import MinimalSchema
 from Products.Archetypes.BaseContent import BaseContent
 from Products.Archetypes.BaseContent import BaseContentMixin
-from Products.Archetypes.BaseContent import BaseSchema
 from Products.Archetypes.BaseFolder import BaseFolder
 from Products.Archetypes.BaseFolder import BaseFolderMixin
-from Products.Archetypes.BaseFolder import BaseFolderSchema
 from Products.Archetypes.BaseBTreeFolder import BaseBTreeFolder
-from Products.Archetypes.BaseBTreeFolder import BaseBTreeFolderSchema
 from Products.Archetypes.OrderedBaseFolder import OrderedBaseFolder
 from Products.Archetypes.ExtensibleMetadata import ExtensibleMetadata
+# base class schemata instances
+from Products.Archetypes.BaseObject import MinimalSchema
+from Products.Archetypes.BaseContent import BaseSchema
+from Products.Archetypes.BaseFolder import BaseFolderSchema
+from Products.Archetypes.BaseBTreeFolder import BaseBTreeFolderSchema
 from Products.Archetypes.ExtensibleMetadata import ExtensibleMetadataSchema
+# schemata classes
 from Products.Archetypes.Schema import BasicSchema
 from Products.Archetypes.Schema import Schema
 from Products.Archetypes.Schema import MetadataSchema
 from Products.Archetypes.Schema import ManagedSchema
 from Products.Archetypes.Schema.Composite import CompositeSchema
 from Products.Archetypes.Schema.Facade import FacadeMetadataSchema
+from Products.Archetypes.VariableSchemaSupport import VariableSchemaSupport
+# marshaller
 from Products.Archetypes.Marshall import PrimaryFieldMarshaller
 from Products.Archetypes.Marshall import RFC822Marshaller
+# fields
 from Products.Archetypes.Field import *
+# widgets
 from Products.Archetypes.Widget import *
+# storage
 from Products.Archetypes.Storage import *
+from Products.Archetypes.AggregatedStorage import AggregatedStorage
+from Products.Archetypes.SQLStorage import BaseSQLStorage
+from Products.Archetypes.SQLStorage import GadflySQLStorage
+from Products.Archetypes.SQLStorage import MySQLSQLStorage
+from Products.Archetypes.SQLStorage import PostgreSQLStorage
+from Products.Archetypes.SQLStorage import SQLServerStorage
+# misc
 from Products.Archetypes.utils import DisplayList
 from Products.Archetypes.ClassGen import AT_GENERATE_METHOD
 
