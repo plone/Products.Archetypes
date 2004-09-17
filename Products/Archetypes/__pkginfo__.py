4fedfe08b0c25f702757d25d25460ef3d3f90a98
Christian 'Tiran' Heimes <lists@cheimes.de>
2004-09-17 14:46:33 +0000

    Preparing Archetypes 1.3.0-rc3
    svn path=/generator/trunk/; revision=3043

--- Products/Archetypes/__pkginfo__.py
+++ Products/Archetypes/__pkginfo__.py
@@ -1,9 +1,9 @@
 
 modname = 'generator'
-version = '1.3.0-5'
-numversion = (1, 3, 0, 5)
+version = '1.3.0-6'
+numversion = (1, 3, 0, 6)
 at_versions = ('1.3.0-beta3', '1.3.0-beta4', '1.3.0-beta5', '1.3.0-rc1',
-    '1.3.0-rc2',
+    '1.3.0-rc2', '1.3.0-rc3',
     )
 
 license = 'GPL'
