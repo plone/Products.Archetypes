f22182ad4d9fd15b5d317c7d72c0a3420478bc57
Christian 'Tiran' Heimes <lists@cheimes.de>
2004-10-10 16:48:48 +0000

    Fixed [ 1042076 ] missing import in SQLMethod.py: atoi and time were missing
    svn path=/Archetypes/branches/release-1_4-branch/; revision=3150

--- Products/Archetypes/public.py
+++ Products/Archetypes/public.py
@@ -1,4 +1,7 @@
 """
 """
 from Products.Archetypes.atapi import *
+from Products.Archetypes.lib.logging import deprecated
+deprecated("`from Products.Archetypes.public import *` is deprecated. Please "
+           "use atapi instead of public!", level=4)
 
