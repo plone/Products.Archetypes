fdf116ae1c68333dd065cb1050374ce3d329bade
Christian 'Tiran' Heimes <lists@cheimes.de>
2004-10-10 16:48:25 +0000

    Fixed [ 1042076 ] missing import in SQLMethod.py: atoi and time were missing
    svn path=/Archetypes/branches/release-1_3-branch/; revision=3149

---
+++ Products/Archetypes/BaseUnit.py
@@ -0,0 +1,235 @@
+from types import StringType
+
+from Products.Archetypes.interfaces.base import IBaseUnit
+from Products.Archetypes.config import *
+
+from AccessControl import ClassSecurityInfo
+from Acquisition import aq_parent
+from Globals import InitializeClass
+from OFS.Image import File
+from Products.CMFCore import CMFCorePermissions
+from Products.CMFCore.utils import getToolByName
+from Products.MimetypesRegistry.interfaces import IMimetypesRegistry, IMimetype
+from Products.PortalTransforms.interfaces import idatastream
+#from Products.MimetypesRegistry.mime_types import text_plain, \
+#     application_octet_stream
+from webdav.WriteLockInterface import WriteLockInterface
+
+class BaseUnit(File):
+    __implements__ = WriteLockInterface, IBaseUnit
+    isUnit = 1
+
+    security = ClassSecurityInfo()
+
+    def __init__(self, name, file='', instance=None, **kw):
+        self.id = self.__name__ = name
+        self.update(file, instance, **kw)
+        
+    def __setstate__(self, dict):
+        mimetype = dict.get('mimetype', None)
+        if IMimetype.isImplementedBy(mimetype):
+            dict['mimetype'] = str(mimetype)
+            dict['binary'] = not not mimetype.binary
+        assert(dict.has_key('mimetype'), 'no mimetype in setstate dict')
+        assert(dict.has_key('binary'), 'no binary in setstate dict')
+        File.__setstate__(self, dict)
+
+    def update(self, data, instance, **kw):
+        #Convert from str to unicode as needed
+        mimetype = kw.get('mimetype', None)
+        filename = kw.get('filename', None)
+        encoding = kw.get('encoding', None)
+
+        adapter = getToolByName(instance, 'mimetypes_registry', None)
+        if not IMimetypesRegistry.isImplementedBy(adapter):
+            raise RuntimeError, \
+                '%s(%s) is not a valid mimetype registry: %s(%s)' % \
+                (repr(adapter), adapter.__class__, repr(instance), aq_parent(instance))
+        data, filename, mimetype = adapter(data, **kw)
+
+        assert mimetype
+        self.mimetype = str(mimetype)
+        self.binary = mimetype.binary
+        if not self.isBinary():
+            assert type(data) is type(u'')
+            if encoding is None:
+                try:
+                    encoding = adapter.guess_encoding(data)
+                except UnboundLocalError:
+                    # adapter is not defined, we are in object creation
+                    import site
+                    encoding = site.encoding
+            self.original_encoding = encoding
+        else:
+            self.original_encoding = None
+        self.raw  = data
+        self.size = len(data)
+        # taking care of stupid IE
+        self.setFilename(filename)
+
+    def transform(self, instance, mt):
+        """Takes a mimetype so object.foo.transform('text/plain') should return
+        a plain text version of the raw content
+
+        return None if no data or if data is untranformable to desired output
+        mime type
+        """
+        encoding = self.original_encoding
+        orig = self.getRaw(encoding)
+        if not orig:
+            return None
+
+        #on ZODB Transaction commit there is by specification
+        #no acquisition context. If it is not present, take
+        #the untransformed getRaw, this is necessary for
+        #being used with APE
+        # Also don't break if transform was applied with a stale instance
+        # from the catalog while rebuilding the catalog
+        if not instance or not hasattr(instance, 'aq_parent'):
+            return orig
+
+        transformer = getToolByName(instance, 'portal_transforms')
+        data = transformer.convertTo(mt, orig, object=self, usedby=self.id,
+                                     context=instance,
+                                     mimetype=self.mimetype,
+                                     filename=self.filename)
+
+        if data:
+            assert idatastream.isImplementedBy(data)
+            _data = data.getData()
+            instance.addSubObjects(data.getSubObjects())
+            portal_encoding = self.portalEncoding(instance)
+            encoding = data.getMetadata().get("encoding") or encoding \
+                       or portal_encoding
+            if portal_encoding != encoding:
+                _data = unicode(_data, encoding).encode(portal_encoding)
+            return _data
+
+        # we have not been able to transform data
+        # return the raw data if it's not binary data
+        # FIXME: is this really the behaviour we want ?
+        if not self.isBinary():
+            portal_encoding = self.portalEncoding(instance)
+            if portal_encoding != encoding:
+                orig = self.getRaw(portal_encoding)
+            return orig
+
+        return None
+
+    def __str__(self):
+        return self.getRaw()
+
+    __call__ = __str__
+
+    def __len__(self):
+        return self.get_size()
+
+    def isBinary(self):
+        """return true if this contains a binary value, else false"""
+        return self.binary
+
+    # File handling
+    def get_size(self):
+        return self.size
+
+    def getRaw(self, encoding=None, instance=None):
+        """return encoded raw value"""
+        if self.isBinary():
+            return self.raw
+        # FIXME: backward compat, non binary data
+        # should always be stored as unicode
+        if not type(self.raw) is type(u''):
+            return self.raw
+        if encoding is None:
+            if instance is None:
+                encoding ='UTF-8'
+            else:
+                # FIXME: fallback to portal encoding or original encoding ?
+                encoding = self.portalEncoding(instance)
+        return self.raw.encode(encoding)
+
+    def portalEncoding(self, instance):
+        """return the default portal encoding, using an external python script
+        (look the archetypes skin directory for the default implementation)
+        """
+        try:
+            return instance.getCharset()
+        except AttributeError:
+            # that occurs during object initialization
+            # (no acquisition wrapper)
+            return 'UTF8'
+
+    def getContentType(self):
+        """return the mimetype string for this BU"""
+        return self.mimetype
+
+    def setContentType(self, instance, value):
+        """
+        """
+        mtr = getToolByName(instance, 'mimetypes_registry')
+        if not IMimetypesRegistry.isImplementedBy(mtr):
+            raise RuntimeError('%s(%s) is not a valid mimetype registry' % \
+                               (repr(mtr), repr(mtr.__class__)))
+        result = mtr.lookup(value)
+        if not result:
+            raise ValueError('Unknown mime type %s' % value)
+        mimetype = result[0]
+        self.mimetype = str(mimetype)
+        self.binary = mimetype.binary
+
+    def content_type(self):
+        return self.getContentType()
+
+    def getFilename(self):
+        return self.filename
+
+    def setFilename(self, filename):
+        """
+        """
+        if type(filename) is StringType:
+            self.filename = filename.split("\\")[-1]
+        else:
+            self.filename = filename
+
+    ### index_html
+    security.declareProtected(CMFCorePermissions.View, "index_html")
+    def index_html(self, REQUEST, RESPONSE):
+        """download method"""
+        filename = self.filename
+        if self.filename:
+            #print self.filename
+            RESPONSE.setHeader('Content-Disposition',
+                               'attachment; filename=%s' % self.getFilename())
+        RESPONSE.setHeader('Content-Type', self.getContentType())
+        RESPONSE.setHeader('Content-Length', self.get_size())
+
+        RESPONSE.write(self.getRaw(encoding=self.original_encoding))
+        return ''
+
+    ### webDAV me this, webDAV me that
+    security.declareProtected( CMFCorePermissions.ModifyPortalContent, 'PUT' )
+    def PUT(self, REQUEST, RESPONSE):
+        """Handle HTTP PUT requests"""
+        self.dav__init(REQUEST, RESPONSE)
+        self.dav__simpleifhandler(REQUEST, RESPONSE, refresh=1)
+        mimetype=REQUEST.get_header('Content-Type', None)
+
+        file=REQUEST['BODYFILE']
+        data = file.read()
+
+        self.update(data, self.aq_parent, mimetype=mimetype)
+
+        self.aq_parent.reindexObject()
+        RESPONSE.setStatus(204)
+        return RESPONSE
+
+    def manage_FTPget(self, REQUEST, RESPONSE):
+        "Get the raw content for this unit(also used for the WebDAV SRC)"
+        RESPONSE.setHeader('Content-Type', self.getContentType())
+        RESPONSE.setHeader('Content-Length', self.get_size())
+        return self.getRaw(encoding=self.original_encoding)
+
+InitializeClass(BaseUnit)
+
+# Backward-compatibility. Should eventually go away after 1.3-final.
+newBaseUnit = BaseUnit
