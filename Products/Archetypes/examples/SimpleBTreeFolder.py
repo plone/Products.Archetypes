97d0423ab80acd144c084a70add4bdf71a07c617
Jens 'jensens' Klein <jens.klein@jensquadrat.de>
2004-11-01 20:17:07 +0000

    wrong version.txt - again :(
    svn path=/Archetypes/tags/Archetypes-1.3.1-rc2/; revision=3253

--- Products/Archetypes/examples/SimpleBTreeFolder.py
+++
@@ -1,9 +0,0 @@
-from Products.Archetypes.public import *
-
-schema = BaseSchema
-
-class SimpleBTreeFolder(BaseBTreeFolder):
-    """A simple folderish archetype"""
-    schema = schema
-
-registerType(SimpleBTreeFolder)
