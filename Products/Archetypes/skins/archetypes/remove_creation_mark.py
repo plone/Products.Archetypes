cfa2a5d4943e974e535124eabc0b56017d49cade
Jens 'jensens' Klein <jens.klein@jensquadrat.de>
2004-11-06 00:19:43 +0000

    missing commit, rewind the tagging
    svn path=/Archetypes/tags/Archetypes-1.3.1-rc3/; revision=3302

--- Products/Archetypes/skins/archetypes/remove_creation_mark.py
+++
@@ -1,9 +0,0 @@
-##parameters=id=None
-SESSION = context.REQUEST.SESSION
-id = id and id or context.getId()
-cflag = SESSION.get('__creation_flag__', {})
-if cflag.has_key(id):
-    del cflag[id]
-if SESSION.has_key(id):
-    del SESSION[id]
-SESSION.set('__creation_flag__', cflag)
