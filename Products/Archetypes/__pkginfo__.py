66d0931c9267cfaab4ecd9c7d555e59235b94119
Jens 'jensens' Klein <jens.klein@jensquadrat.de>
2004-12-06 12:40:52 +0000

    modified the handling of version retrieving should now get it from the single place, the file version.txt. the whole pkginfo needs a refactoring in 1.4.
    svn path=/generator/trunk/; revision=3367

--- Products/Archetypes/__pkginfo__.py
+++ Products/Archetypes/__pkginfo__.py
@@ -1,15 +1,45 @@
+from Products import generator as PRODUCT
 
-modname = 'generator'
-version = '1.3.0-9'
-numversion = (1, 3, 0, 9)
-at_versions = ('1.3.0-beta3', '1.3.0-beta4', '1.3.0-beta5', '1.3.0-rc1',
-    '1.3.0-rc2', '1.3.0-rc3', '1.3.0-rc4', '1.3.0-final', '1.3.1-rc1',
+version=PRODUCT.__version__
+modname=PRODUCT.__name__
+
+# (major, minor, patchlevel, release info) where release info is:
+# -99 for alpha, -49 for beta, -19 for rc and 0 for final
+# increment the release info number by one e.g. -98 for alpha2
+
+major, minor, bugfix =  version.split('.')
+bugfix, release = bugfix.split('-')
+
+relinfo=-99 #alpha
+if 'beta' in release:
+    relinfo=-49
+if 'rc' in release:
+    relinfo=-19
+if 'final' in release:
+    relinfo=0
+
+numversion = (int(major), int(minor), int(bugfix), relinfo)
+
+at_versions = (
+    '1.3.0-beta3',
+    '1.3.0-beta4',
+    '1.3.0-beta5',
+    '1.3.0-rc1',
+    '1.3.0-rc2',
+    '1.3.0-rc3',
+    '1.3.0-rc4',
+    '1.3.0-final',
+    '1.3.1-rc1',
+    '1.3.1-rc2',
+    '1.3.1-rc3',
+    '1.3.1-rc4',
+    '1.3.1-final',
+    ###MARKERFORATRELEASESCRIPT###
     )
-
 license = 'GPL'
 copyright = '''Benjamin Saller (c) 2003'''
 
-author = "Archetypes developpement team"
+author = "Archetypes developement team"
 author_email = "archetypes-devel@lists.sourceforge.net"
 
 short_desc = "Widget generator for Archetypes"
