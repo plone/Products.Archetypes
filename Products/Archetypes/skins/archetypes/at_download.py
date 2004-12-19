f054f05047517f7ac78eec84b7e74d376600d2d6
Jens 'jensens' Klein <jens.klein@jensquadrat.de>
2004-12-19 19:10:24 +0000

    Use method getFilename in at_download.pt instead of direkt attribute access, which ignores BaseUnit.
    svn path=/Archetypes/branches/release-1_3-branch/; revision=3392

--- Products/Archetypes/skins/archetypes/at_download.py
+++ Products/Archetypes/skins/archetypes/at_download.py
@@ -7,17 +7,13 @@
 ##bind subpath=traverse_subpath
 REQUEST  = context.REQUEST
 RESPONSE = REQUEST.RESPONSE
-
-filename = getattr(context, 'filename', context.getId())
-RESPONSE.setHeader('Content-Disposition',
-                   'attachment; filename=%s' % filename)
-
 if hasattr(context.aq_explicit, 'data'):
     # handle OFS.Image.File and OFS.Image.Image
-    return context.index_html(REQUEST, RESPONSE)
+    data = str(context.data)
 else:
     data = str(context)
-
+RESPONSE.setHeader('Content-Disposition',
+                   'attachment; filename="%s"' % context.getFilename())
 RESPONSE.setHeader('Content-Type', context.getContentType())
 RESPONSE.setHeader('Content-Length', context.get_size())
 RESPONSE.write(data)
