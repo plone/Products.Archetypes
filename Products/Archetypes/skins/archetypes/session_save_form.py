b72704c3d8e261b8be8250828127d8f972d94359
Rocky Burt <rocky@serverzen.com>
2005-02-04 01:01:07 +0000

    Based branch off of wrong source branch.
    svn path=/Archetypes/branches/rocky-skins_branch/; revision=3524

--- Products/Archetypes/skins/archetypes/session_save_form.py
+++
@@ -1,9 +0,0 @@
-REQUEST = context.REQUEST
-# hey, don't forget to increment object number for sessions
-form_data = {'HTTP_REFERER':REQUEST.get('lastest_referer', None)}
-to_store = [f for f in context.Schema().values() if f.type != 'computed']
-for field in to_store:
-    fieldname = field.getName()
-    if REQUEST.has_key(fieldname):
-        form_data[fieldname] = REQUEST.get(fieldname)
-REQUEST.SESSION.set(context.getId(), form_data)
