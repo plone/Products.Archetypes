0338878d9adbb2a02ea597cfa181ee1d0594f3bb
Christian 'Tiran' Heimes <lists@cheimes.de>
2005-02-13 17:27:01 +0000

    Somehow the copyright headers got lost ...
    svn path=/Archetypes/branches/release-1_4-branch/; revision=3586

--- Products/Archetypes/tests/atsitetestcase.py
+++ Products/Archetypes/tests/atsitetestcase.py
@@ -1,9 +1,41 @@
+# -*- coding: UTF-8 -*-
+################################################################################
+#
+# Copyright (c) 2002-2005, Benjamin Saller <bcsaller@ideasuite.com>, and
+#                              the respective authors. All rights reserved.
+# For a list of Archetypes contributors see docs/CREDITS.txt.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of the author nor the names of its contributors may be used
+#   to endorse or promote products derived from this software without specific
+#   prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+################################################################################
+"""
+"""
+__author__ = "Christian Heimes"
+
 from Testing import ZopeTestCase
 
 from Testing.ZopeTestCase.functional import Functional
 from Products.CMFTestCase import CMFTestCase
 from Products.CMFTestCase.setup import portal_name, portal_owner
 from Products.Archetypes.tests.attestcase import ATTestCase
+from AccessControl.SecurityManagement import newSecurityManager
+from AccessControl.SecurityManagement import noSecurityManager
+from Acquisition import aq_base
 
 # setup a CMF site 
 CMFTestCase.setupCMFSite()
@@ -34,10 +66,6 @@ class ATFunctionalSiteTestCase(Functional, ATSiteTestCase):
 
 import time
 from StringIO import StringIO
-
-from AccessControl.SecurityManagement import newSecurityManager
-from AccessControl.SecurityManagement import noSecurityManager
-from Acquisition import aq_base
 
 from Products.CMFCore.utils import getToolByName
 from Products.Archetypes.config import PKG_NAME
--- Products/Archetypes/tests/attestcase.py
+++ Products/Archetypes/tests/attestcase.py
@@ -1,7 +1,31 @@
+# -*- coding: UTF-8 -*-
+################################################################################
 #
-# ArchetypesTestCase
+# Copyright (c) 2002-2005, Benjamin Saller <bcsaller@ideasuite.com>, and
+#                              the respective authors. All rights reserved.
+# For a list of Archetypes contributors see docs/CREDITS.txt.
 #
-# $Id: ArchetypesTestCase.py 3433 2005-01-17 00:31:49Z tiran $
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of the author nor the names of its contributors may be used
+#   to endorse or promote products derived from this software without specific
+#   prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+################################################################################
+"""
+"""
+__author__ = "Christian Heimes"
 
 from Testing import ZopeTestCase
 from Testing.ZopeTestCase.functional import Functional
