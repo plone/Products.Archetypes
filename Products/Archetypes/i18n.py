a862cd727f63f9846cfca4aaffb2dcd02162d085
Jens 'jensens' Klein <jens.klein@jensquadrat.de>
2005-04-24 17:29:08 +0000

    fixed translation encoding problems, see http://plone.org/collector/3972, thx to [naro]. removed checks against Localizer (whoever uses it should move to PlacelessTranslatonService. removed check against Zope 2.5.
    svn path=/generator/trunk/; revision=4100

--- Products/Archetypes/i18n.py
+++ Products/Archetypes/i18n.py
@@ -1,39 +1,37 @@
-try:
-    # XXX Depends on 2.6
-    from Products.PageTemplates.GlobalTranslationService import getGlobalTranslationService, \
-         DummyTranslationService
-except ImportError:
-    class DummyTranslationService:
-        """ A very very dummy translation service """
-        pass
-
-    def getGlobalTranslationService():
-        return DummyTranslationService
+""" IMPORTANT: this module is unusable before initialize is called
+    this must be so because we want to make sure all products
+    (eg, whatever translation service we're supposed to use)
+    is already there and ready
+"""    
+from Products.PageTemplates.GlobalTranslationService import getGlobalTranslationService, \
+     DummyTranslationService
 
 service = None
 translate = None
 
-def translate_wrapper(domain, msgid, mapping=None, context=None, target_language=None, default=None):
+def translate_wrapper(domain, msgid, mapping=None, context=None, 
+                      target_language=None, default=None):
     # wrapper for calling the translate() method with a fallback value
 
-    try:
-        res = service.translate(domain, msgid, mapping=mapping, context=context, target_language=target_language, default=default)
-    except TypeError:
-        #Localizer does not take a default param
-        res = service.translate(domain, msgid, mapping=mapping, context=context, target_language=target_language)
+    if hasattr(service, 'utranslate'):
+        res = service.utranslate(domain, msgid, mapping=mapping, context=context, 
+                                 target_language=target_language, 
+                                 default=default)
+    else:
+        res = service.translate(domain, msgid, mapping=mapping, context=context, 
+                                target_language=target_language, 
+                                default=default)
 
     if res is None or res is msgid:
         return default
     return res
 
-def null_translate(domain, msgid, mapping=None, context=None, target_language=None, default=None):
+def null_translate(domain, msgid, mapping=None, context=None, 
+                   target_language=None, default=None):
     return default
 
 def initialize():
-    # IMPORTANT: this module is unusable before this is called
-    # this must be so because we want to make sure all products
-    # (eg, whatever translation service we're supposed to use)
-    # is already there and ready
+    """ must be calles after Products are there and ready """
     global service, translate
     service = getGlobalTranslationService()
     if service is DummyTranslationService:
@@ -44,7 +42,8 @@ def initialize():
     else:
         translate = translate_wrapper
 
-def initial_translate(domain, msgid, mapping=None, context=None, target_language=None, default=None):
+def initial_translate(domain, msgid, mapping=None, context=None, 
+                      target_language=None, default=None):
     initialize()
     return translate(domain, msgid, mapping, context, target_language, default)
 
