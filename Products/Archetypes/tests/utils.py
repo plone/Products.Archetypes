c602bb88ba791b743972d4a05d0c5e35b3ed4c3d
Alec Mitchell <apm13@columbia.edu>
2005-07-19 22:00:21 +0000

    dded ftests for id autogeneration.
    svn path=/Archetypes/branches/release-1_3-branch/; revision=4621

--- Products/Archetypes/tests/utils.py
+++ Products/Archetypes/tests/utils.py
@@ -28,7 +28,13 @@
 
 import re
 from TAL import ndiff
-from Globals import package_home
+from Globals import InitializeClass, package_home
+from UserDict import UserDict
+import ExtensionClass
+from Acquisition import Implicit
+from AccessControl import ClassSecurityInfo
+from ZPublisher.BeforeTraverse import registerBeforeTraverse
+from Persistence import Persistent
 
 from Products.Archetypes.atapi import registerType
 from Products.Archetypes.atapi import process_types
@@ -205,3 +211,77 @@ def unwrap_method(klass, name):
     new_method = getattr(klass, orig_name)
     delattr(klass, orig_name)
     setattr(klass, name, new_method)
+
+class FakeRequestSession(ExtensionClass.Base, UserDict):
+    """Dummy dict like object with set method for SESSION and REQUEST
+    """
+    security = ClassSecurityInfo()
+    security.setDefaultAccess('allow')
+    security.declareObjectPublic()
+    
+    def __init__(self):
+        UserDict.__init__(self)
+        # add a dummy because request mustn't be empty for test
+        # like 'if REQUEST:'
+        self['__dummy__'] = None
+    
+    def __nonzero__(self):
+        return True
+    
+    def set(self, key, value):
+        self[key] = value
+
+InitializeClass(FakeRequestSession)
+FakeRequestSession()
+
+class DummySessionDataManager(Implicit):
+    """Dummy sdm for sessioning
+    
+    Uses traversal hooks to add the SESSION object as lazy item to the request
+    """
+    security = ClassSecurityInfo()
+    security.setDefaultAccess('allow')
+    security.declareObjectPublic()
+    
+    id = 'session_data_manager'
+    
+    def __init__(self):
+        self.session = FakeRequestSession()
+        
+    def manage_afterAdd(self, item, container):
+        """Register traversal hooks to add SESSION to request
+        """
+        parent = self.aq_inner.aq_parent
+        hook = DummySDMTraverseHook()
+        registerBeforeTraverse(parent, hook, 'SessionDataManager', 50)
+
+    def getSessionData(self, create=1):
+        """ """
+        return self.session
+    
+    def hasSessionData(self):
+        """ """
+        return True
+    
+    def getSessionDataByKey(self, key):
+        """ """
+        return self.session
+    
+    def getBrowserIdManager(self):
+        """ """
+        # dummy
+        return self
+
+InitializeClass(DummySessionDataManager)
+
+class DummySDMTraverseHook(Persistent):
+    """Traversal hook for dummy sessions
+    
+    See Products.Sessions.SessionDataManager.SessionDataManagerTraverser
+    """
+    
+    def __call__(self, container, request):
+        id = DummySessionDataManager.id
+        sdm = getattr(container, id)
+        getSessionData = sdm.getSessionData
+        request.set_lazy('SESSION', getSessionData)
