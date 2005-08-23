003c0e16c7a112cb4c1daf7023dd9e6728b15076
Stefan H. Holek <stefan@epy.co.at>
2005-08-23 07:56:26 +0000

    Make the reST tests work in Zope 2.8 as well.
    svn path=/Archetypes/branches/release-1_3-branch/; revision=4809

--- Products/Archetypes/tests/test_baseunit.py
+++ Products/Archetypes/tests/test_baseunit.py
@@ -37,6 +37,7 @@ import os
 
 from Products.Archetypes.tests.atsitetestcase import ATSiteTestCase
 from Products.Archetypes.tests.utils import PACKAGE_HOME
+from Products.Archetypes.tests.utils import ZOPE28
 from Products.Archetypes.tests.utils import normalize_html
 from Products.Archetypes.tests.utils import gen_class
 from Products.Archetypes.atapi import *
@@ -82,6 +83,8 @@ input_files = glob.glob(os.path.join(PACKAGE_HOME, "input", "rest*.rst"))
 for f in input_files:
     fname = os.path.split(f)[1]
     outname = os.path.join(PACKAGE_HOME, "output", '%s.out' % fname.split('.')[0])
+    if ZOPE28:
+        outname += '.zope28'
 
     class BaseUnitTestSubclass(BaseUnitTest):
         input = f
