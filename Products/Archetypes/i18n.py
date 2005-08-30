2289584b9f9cc8d5b9a0841804d9daa21f452a0f
dtremea <dtremea@localhost>
2005-08-30 04:17:25 +0000

    Fixed a long stand issue which prevented you from override the language used to translate Label and Description. What do you think the 'target_language' was supposed created to? ;-) Unfortunately there's no tests et al for this AT corner, so blame me if anything goes wrong with your custom code.
    svn path=/generator/trunk/; revision=4878

--- Products/Archetypes/i18n.py
+++ Products/Archetypes/i18n.py
@@ -2,47 +2,47 @@
     this must be so because we want to make sure all products
     (eg, whatever translation service we're supposed to use)
     is already there and ready
-"""    
-from Products.PageTemplates.GlobalTranslationService import getGlobalTranslationService, \
-     DummyTranslationService
+"""
+from Products.PageTemplates.GlobalTranslationService import \
+     getGlobalTranslationService, DummyTranslationService
 
 service = None
 translate = None
 
-def translate_wrapper(domain, msgid, mapping=None, context=None, 
+def translate_wrapper(domain, msgid, mapping=None, context=None,
                       target_language=None, default=None):
-    """ wrapper for calling the translate() method with a fallback value """
+    """Wrapper for calling the translate() method with a fallback value."""
 
     if hasattr(service, 'utranslate'):
-        res = service.utranslate(domain, msgid, mapping=mapping, context=context, 
-                                 target_language=target_language, 
+        res = service.utranslate(domain, msgid, mapping=mapping, context=context,
+                                 target_language=target_language,
                                  default=default)
     else:
-        res = service.translate(domain, msgid, mapping=mapping, context=context, 
-                                target_language=target_language, 
+        res = service.translate(domain, msgid, mapping=mapping, context=context,
+                                target_language=target_language,
                                 default=default)
 
     if res is None or res is msgid:
         return default
     return res
 
-def null_translate(domain, msgid, mapping=None, context=None, 
+def null_translate(domain, msgid, mapping=None, context=None,
                    target_language=None, default=None):
     return default
 
 def initialize():
-    """ must be called after Products are there and ready """
+    """Must be called after Products are there and ready."""
     global service, translate
     service = getGlobalTranslationService()
     if service is DummyTranslationService:
         translate = null_translate
     elif hasattr(service, '_fallbacks'):
-        # it accepts the "default" argument
+        # It accepts the "default" argument
         translate = service.translate
     else:
         translate = translate_wrapper
 
-def initial_translate(domain, msgid, mapping=None, context=None, 
+def initial_translate(domain, msgid, mapping=None, context=None,
                       target_language=None, default=None):
     initialize()
     return translate(domain, msgid, mapping, context, target_language, default)
