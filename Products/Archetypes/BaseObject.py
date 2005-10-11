712e6fbdbfb4a4c4e14a28632ff927772d5964a5
Laurence Rowe <l@lrowe.co.uk>
2005-10-11 13:21:36 +0000

    * Fixed at_post_create_script hook to only be called once (was previously     called after every edit) and added an at_post_edit_script hook to be     called on every subsequent edit (but not the first create/edit).     [elro]
    svn path=/Archetypes/branches/release-1_3-branch/; revision=5024

--- Products/Archetypes/BaseObject.py
+++ Products/Archetypes/BaseObject.py
@@ -624,6 +624,22 @@ class BaseObject(Referenceable):
         self.unmarkCreationFlag()
         if self._at_rename_after_creation and is_new_object:
             self._renameAfterCreation(check_auto_id=True)
+        
+        # Post create/edit hooks
+        if is_new_object:
+            self.at_post_create_script()
+        else:
+            self.at_post_edit_script()
+
+    # This method is only called once after object creation.
+    security.declareProtected(CMFCorePermissions.View, 'at_post_create_script')
+    def at_post_create_script(self):
+        pass
+
+    # This method is called after every subsequent edit
+    security.declareProtected(CMFCorePermissions.View, 'at_post_edit_script')
+    def at_post_edit_script(self):
+        pass
 
     security.declareProtected(CMFCorePermissions.ModifyPortalContent,
                               'markCreationFlag')
@@ -652,15 +668,6 @@ class BaseObject(Referenceable):
         """
         if shasattr(aq_inner(self), '_at_creation_flag'):
             self._at_creation_flag = False
-        if shasattr(self, 'at_post_create_script'):
-            post_create = getattr(self, 'at_post_create_script', None)
-            if post_create is not None:
-                try:
-                    post_create()
-                except TypeError:
-                    log("unmarkCreationFlag: at_post_create_script not "
-                        "callable")
-                    pass
 
     security.declareProtected(CMFCorePermissions.ModifyPortalContent,
                               'checkCreationFlag')
