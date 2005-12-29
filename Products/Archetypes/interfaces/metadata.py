88359b56560892e177ecf84d1033002948c598bb
Jens 'jensens' Klein <jens.klein@jensquadrat.de>
2005-12-29 15:51:53 +0000

    wrong branch
    svn path=/Archetypes/tags/Archetypes-1.3.6-RC1/; revision=5364

--- Products/Archetypes/interfaces/metadata.py
+++
@@ -1,8 +0,0 @@
-from Interface import Interface, Attribute
-from Products.CMFCore.interfaces.DublinCore import DublinCore
-from Products.CMFCore.interfaces.DublinCore import CatalogableDublinCore
-from Products.CMFCore.interfaces.DublinCore import MutableDublinCore
-
-class IExtensibleMetadata(DublinCore, CatalogableDublinCore, MutableDublinCore):
-    """ Archetypes implementation of DublinCore metadata """
-    pass
