88ded2346d0f1a6a3a87c10cd72e3326c6a5fac8
Benjamin Saller <bcsaller@objectrealms.net>
2006-03-09 06:27:51 +0000

    *prep for NOLA
    svn path=/Archetypes/branches/objectrealms-bling/; revision=6139

--- Products/Archetypes/Renderer.py
+++ Products/Archetypes/Renderer.py
@@ -11,10 +11,10 @@ class ArchetypesRenderer(BaseRenderer):
     # XXX it says it's implementing layer but it doesn't implement the required
     # methods!
     #__implements__ = ILayer
-    
+
     security = ClassSecurityInfo()
     # XXX FIXME more security
-    
+
     def setupContext(self, field_name, mode, widget, instance, field, \
                      accessor, **kwargs):
 
@@ -26,8 +26,8 @@ class ArchetypesRenderer(BaseRenderer):
             frame = frame.f_back
         if context is _marker:
             raise RuntimeError, 'Context not found'
-        
-        # for editing of multiple AT-based content at once we might want to 
+
+        # for editing of multiple AT-based content at once we might want to
         # prefix the field-name.
         if 'fieldprefix' in kwargs:
             field_name = '%s%s' % (kwargs['fieldprefix'], field_name)
@@ -51,3 +51,67 @@ class ArchetypesRenderer(BaseRenderer):
 InitializeClass(ArchetypesRenderer)
 
 renderer = ArchetypesRenderer()
+
+from StringIO import StringIO
+from Products.PageTemplates.Expressions import getEngine
+engine = getEngine()
+from TAL.TALInterpreter import TALInterpreter
+from Products.CMFCore.Expression import createExprContext
+from Products.CMFPlone.utils import IndexIterator
+
+class AJAXRenderer(BaseRenderer):
+    security = ClassSecurityInfo()
+
+    def render(self, field_name, mode, widget, instance=None,
+               field=None, accessor=None, **kwargs):
+        if field is None:
+            field = instance.Schema()[field_name]
+
+        if accessor is None:
+            accessor = field.getAccessor(instance)
+
+        context = self.setupContext(field_name, mode, widget,
+                                    instance, field, accessor,
+                                    **kwargs)
+        macro = widget(mode, instance, context)
+        buffer = StringIO()
+        # evaluate global_defines (to load all the vars we depend on)
+        #defines = instance.unrestrictedTraverse("global_defines").macros()['defines']
+        #TALInterpreter(macro, {} context, buffer)()
+        # Now eval the macro
+        TALInterpreter(macro, {}, context, buffer)()
+        return buffer.getvalue()
+
+
+    def setupContext(self, field_name, mode, widget, instance, field, \
+                     accessor, **kwargs):
+
+        # create a new context object
+        folder = instance.aq_parent
+        utool  = instance.portal_url
+        portal = utool.getPortalObject()
+
+        context = createExprContext(folder, portal, instance)
+
+        # This seems to be about all we need to fake an AT widgets
+        # "interface".. umm, ugh...
+        widget = ImplicitAcquisitionWrapper(widget, instance)
+        field = ImplicitAcquisitionWrapper(field, instance)
+        context.setLocal('here', instance)
+        context.setLocal('fieldName', field_name)
+        context.setLocal('accessor', accessor)
+        context.setLocal('widget', widget)
+        context.setLocal('field', field)
+        context.setLocal('mode', mode)
+        context.setLocal("utool", utool)
+        context.setLocal("portal", portal)
+        context.setLocal("errors", {})
+        context.setLocal("tabindex", IndexIterator(pos=30000))
+        # This is a hack but shouldn't matter given that its used for
+        # context only in the calls i've seen
+        context.setLocal("template", instance)
+
+        return context
+
+InitializeClass(AJAXRenderer)
+ajax_renderer = AJAXRenderer()
