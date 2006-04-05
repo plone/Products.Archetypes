fa4bd597d8649abcbac967070b7723c676890333
Whit Morriss <whit@openplans.org>
2006-04-05 01:00:56 +0000

    last of the interface conversions
    svn path=/Archetypes/branches/whitmo-iface-aggedon/; revision=6232

--- Products/Archetypes/interfaces/_metadata.py
+++ Products/Archetypes/interfaces/_metadata.py
@@ -1,14 +1,4 @@
-# BBB
-try:
-    from Products.CMFCore.interfaces import CatalogableDublinCore
-except ImportError:
-    from Products.CMFCore.interfaces.DublinCore import DublinCore, CatalogableDublinCore, MutableDublinCore
-    import Products.CMFCore.interfaces as module
-    from Products.Archetypes import utils
-    utils.makeZ2Bridges(module, DublinCore, CatalogableDublinCore, MutableDublinCore)
+### coming soon
 
-from Products.CMFCore.interfaces import DublinCore, CatalogableDublinCore, MutableDublinCore
 
-class IExtensibleMetadata(DublinCore, CatalogableDublinCore, MutableDublinCore):
-    """ Archetypes implementation of DublinCore metadata """
-    pass
+
