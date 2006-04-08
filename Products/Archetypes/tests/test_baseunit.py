9bbabb7ce09b69351aa39c9011468b6859a70681
Stefan H. Holek <stefan@epy.co.at>
2006-04-08 11:05:21 +0000

    Removed reST-test-kludge. test_baseunit.py now fails one test in Zope 2.8 but the solution must be something other than fixing up tests.
    svn path=/Archetypes/branches/1.4/; revision=6245

--- Products/Archetypes/tests/test_baseunit.py
+++ Products/Archetypes/tests/test_baseunit.py
@@ -36,8 +36,6 @@ import glob
 
 from Products.Archetypes.tests.atsitetestcase import ATSiteTestCase
 from Products.Archetypes.tests.utils import PACKAGE_HOME
-from Products.Archetypes.tests.utils import ZOPE28
-from Products.Archetypes.tests.utils import ZOPE29
 from Products.Archetypes.tests.utils import normalize_html
 from Products.Archetypes.tests.utils import gen_class
 from Products.Archetypes.atapi import *
@@ -83,9 +81,6 @@ input_files = glob.glob(os.path.join(PACKAGE_HOME, "input", "rest*.rst"))
 for f in input_files:
     fname = os.path.split(f)[1]
     outname = os.path.join(PACKAGE_HOME, "output", '%s.out' % fname.split('.')[0])
-    # Funkily, Zope 2.9 returned to the pre 2.8 reST behavior...
-    if ZOPE28 and not ZOPE29:
-        outname += '.zope28'
 
     class BaseUnitTestSubclass(BaseUnitTest):
         input = f
