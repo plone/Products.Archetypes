b4ae6e8b455ef89146497862b8eabceaeb01b261
archipelagosvk <archipelagosvk@localhost>
2006-04-26 19:14:43 +0000

    r6317@archipelago:  jfroche | 2006-04-26 13:52:46 +0200  PLIP 145#TTW Locking
     - unlock on Cancel
     - improve UI
     
     
     
    
    svn path=/Archetypes/branches/plip_145_TTWLocking/; revision=6318

---
+++ Products/Archetypes/browser/locking.py
@@ -0,0 +1,65 @@
+from Products.Five import BrowserView
+from zope.event import notify
+from Acquisition import aq_inner
+from Products.Archetypes.events import EditBeginsEvent
+from AccessControl import getSecurityManager
+from Products.Archetypes.interfaces import ILock
+from Products.CMFCore.utils import getToolByName
+from DateTime import DateTime
+
+class LockingView(BrowserView):
+    """
+    Provides helper methodes for locking support
+    """
+
+    def notifyEditBegins(self):
+        """
+        """
+        print "Fired EditBeginsEvent"
+        notify(EditBeginsEvent(aq_inner(self.context)))
+
+    def isLocked(self):
+        """
+        """
+        if not hasattr(self.context, 'wl_isLocked'):
+            return False
+        elif self.context.wl_isLocked():
+            user_id = getSecurityManager().getUser().getId()
+            for lock in self.context.wl_lockValues():
+                if not lock.isValid():
+                    continue # Skip invalid/expired locks
+                creator = lock.getCreator()
+                if creator and creator[1] == user_id:
+                    return False
+            return True
+        return False
+
+    def getLockInformations(self):
+        """
+        """
+        for lock in self.context.wl_lockValues():
+            if not lock.isValid():
+                continue # Skip invalid/expired locks
+            userid = lock.getCreator()[1]
+            lockCreationTime = lock.getModifiedTime()
+            mt = getToolByName(self.context, 'portal_membership', None)
+            pu = getToolByName(self.context, 'portal_url', None)
+            authorPage = "%s/author/%s" % (pu(), userid)
+            if mt:
+                member = mt.getMemberById(userid)
+                if member:
+                    creatorFullname = member.getProperty('fullname') or userid
+            return {'userid': userid, 'creator':creatorFullname, 'lockCreationTime': lockCreationTime, 'authorPage': authorPage}
+
+    def forceUnlock(self):
+        """
+        """
+        locker = ILock(self.context)
+        locker.unlock()
+        self.request.RESPONSE.redirect('%s' % self.context.absolute_url())
+
+
+
+
+
+
