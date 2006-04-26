10ba561f6f38fcdc141461598e052c5f0d2000df
archipelagosvk <archipelagosvk@localhost>
2006-04-26 19:11:14 +0000

    r6309@archipelago:  jfroche | 2006-04-26 00:48:18 +0200  PLIP 145#TTW Locking
     - starting UI integration
     
     
    
    svn path=/Archetypes/branches/plip_145_TTWLocking/; revision=6310

--- Products/Archetypes/browser/locking.py
+++ Products/Archetypes/browser/locking.py
@@ -3,11 +3,15 @@ from zope.event import notify
 from Acquisition import aq_inner
 from Products.Archetypes.events import EditBeginsEvent
 from AccessControl import getSecurityManager
+from Products.Archetypes.interfaces import ILock
+from Products.CMFCore.utils import getToolByName
+from DateTime import DateTime
 
 class LockingView(BrowserView):
     """
     Provides helper methodes for locking support
     """
+
     def notifyEditBegins(self):
         """
         """
@@ -29,4 +33,31 @@ class LockingView(BrowserView):
                     return False
             return True
         return False
+
+    def getLockInformations(self):
+        """
+        """
+        for lock in self.context.wl_lockValues():
+            if not lock.isValid():
+                continue # Skip invalid/expired locks
+            creatorFullname = lock.getCreator()[1]
+            lockCreationTime = lock.getModifiedTime()
+            pts = getToolByName(self.context, 'translation_service', None)
+            mt = getToolByName(self.context, 'portal_membership', None)
+            if mt:
+                member = mt.getMemberById(creatorFullname)
+                if member:
+                    creatorFullname = member.getProperty('fullname') or creatorFullname
+            return {'creator':creatorFullname, 'lockCreationTime': pts.toLocalizedTime(lockCreationTime, 1)}
+
+    def forceUnlock(self):
+        """
+        """
+        locker = ILock(self.context)
+        locker.unlock()
+
+
+
+
+
 
