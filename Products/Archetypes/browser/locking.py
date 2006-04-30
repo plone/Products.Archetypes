32410f46156ed35ef3677214ea8d2c0d7faff6ca
archipelagosvk <archipelagosvk@localhost>
2006-04-30 21:46:20 +0000

    r6352@archipelago:  jfroche | 2006-04-28 19:37:57 +0200  PLIP 145# TTW Locking
     - bug fix for special use case with on unload window
     
    
    svn path=/Archetypes/branches/plip_145_TTWLocking/; revision=6344

--- Products/Archetypes/browser/locking.py
+++ Products/Archetypes/browser/locking.py
@@ -7,6 +7,7 @@ from AccessControl import getSecurityManager
 from Products.Archetypes.interfaces import ILock
 from Products.CMFCore.utils import getToolByName
 from DateTime import DateTime
+from datetime import timedelta
 
 class LockingView(BrowserView):
     """
@@ -16,7 +17,6 @@ class LockingView(BrowserView):
     def notifyEditBegins(self):
         """
         """
-        print "Fired EditBeginsEvent"
         notify(EditBeginsEvent(aq_inner(self.context)))
 
     def notifyEditEnds(self):
@@ -24,21 +24,49 @@ class LockingView(BrowserView):
         """
         notify(EditEndsEvent(aq_inner(self.context)))
 
-    def isLocked(self):
+    def hasLock(self):
+        user_id = getSecurityManager().getUser().getId()
+        for lock in self.context.wl_lockValues():
+            if not lock.isValid():
+                continue # Skip invalid/expired locks
+            creator = lock.getCreator()
+            if creator and creator[1] == user_id:
+                return True
+        return False
+
+    def isLockedForMe(self):
         """
         """
         if not hasattr(self.context, 'wl_isLocked'):
             return False
         elif self.context.wl_isLocked():
-            user_id = getSecurityManager().getUser().getId()
-            for lock in self.context.wl_lockValues():
-                if not lock.isValid():
-                    continue # Skip invalid/expired locks
-                creator = lock.getCreator()
-                if creator and creator[1] == user_id:
-                    return False
-            return True
+            return not self.hasLock()
         return False
+
+    def getNiceTimeDifference(self, baseTime):
+        now=DateTime()
+        days = int(round(now-DateTime(baseTime) ))
+        delta = timedelta(now-DateTime(baseTime))
+        days = delta.days
+        hours = int(delta.seconds/3600)
+        minutes = (delta.seconds - (hours * 3600))/60
+        bylinedate = ""
+        if days == 0:
+            if hours == 0:
+                if delta.seconds < 120:
+                    bylinedate = "1 minute"
+                else:
+                    bylinedate = "%s minutes" % minutes
+            elif hours == 1:
+                bylinedate = "%s hour and %s minutes" % (hours, minutes)
+            else:
+                bylinedate = "%s hours and %s minutes" % (hours, minutes)
+        else:
+            if days == 1:
+                bylinedate = "%s day and %s hours" % (days, hours)
+            else:
+                bylinedate = "%s days and %s hours" % (days, hours)
+        return bylinedate
 
     def getLockInformations(self):
         """
@@ -55,7 +83,11 @@ class LockingView(BrowserView):
                 member = mt.getMemberById(userid)
                 if member:
                     creatorFullname = member.getProperty('fullname') or userid
-            return {'userid': userid, 'creator':creatorFullname, 'lockCreationTime': lockCreationTime, 'authorPage': authorPage}
+            return {'userid': userid, 'creator':creatorFullname,
+                    'lockCreationTime': lockCreationTime,
+                    'authorPage': authorPage,
+                    'timeDifference':self.getNiceTimeDifference(lockCreationTime)
+            }
 
     def forceUnlock(self, redirect=True):
         """
@@ -64,6 +96,12 @@ class LockingView(BrowserView):
         locker.unlock()
         if redirect:
             self.request.RESPONSE.redirect('%s' % self.context.absolute_url())
+
+    def safeUnlock(self):
+        if(self.hasLock()):
+            notify(EditEndsEvent(aq_inner(self.context)))
+
+
 
 
 
