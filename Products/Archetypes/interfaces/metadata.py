24eb80ea82412a9e06c5264db1821e6d2f174eaf
Jens 'jensens' Klein <jens.klein@jensquadrat.de>
2006-06-02 18:36:30 +0000

    once again
    svn path=/Archetypes/tags/Archetypes-1.4.0-RC1/; revision=6530

--- Products/Archetypes/interfaces/metadata.py
+++
@@ -1,5 +0,0 @@
-from Products.CMFCore.interfaces.DublinCore import DublinCore, CatalogableDublinCore, MutableDublinCore
-
-class IExtensibleMetadata(DublinCore, CatalogableDublinCore, MutableDublinCore):
-    """ Archetypes implementation of DublinCore metadata """
-    pass
