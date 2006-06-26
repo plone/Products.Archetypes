84f409f32852a22afa2278e6133bb4c9ef0217cf
Hanno Schlichting <hanno@hannosch.eu>
2006-06-26 14:52:54 +0000

    Renamed sample_content profile to sample_types as it only includes types but no content ;) This profile is not registered globally anymore but only during test runs, as these types aren't meant to be used for anything else.
    svn path=/Archetypes/trunk/; revision=6650

--- Products/Archetypes/tests/atsitetestcase.py
+++ Products/Archetypes/tests/atsitetestcase.py
@@ -1,55 +1,22 @@
-# -*- coding: UTF-8 -*-
-################################################################################
-#
-# Copyright (c) 2002-2005, Benjamin Saller <bcsaller@ideasuite.com>, and
-#                              the respective authors. All rights reserved.
-# For a list of Archetypes contributors see docs/CREDITS.txt.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# * Redistributions of source code must retain the above copyright notice, this
-#   list of conditions and the following disclaimer.
-# * Redistributions in binary form must reproduce the above copyright notice,
-#   this list of conditions and the following disclaimer in the documentation
-#   and/or other materials provided with the distribution.
-# * Neither the name of the author nor the names of its contributors may be used
-#   to endorse or promote products derived from this software without specific
-#   prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
-# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
-# FOR A PARTICULAR PURPOSE.
-#
-################################################################################
-"""
-"""
-__author__ = "Christian Heimes"
-
 from Testing import ZopeTestCase
 
 from Testing.ZopeTestCase.functional import Functional
 from Products.Archetypes.tests import attestcase
-from AccessControl.SecurityManagement import newSecurityManager
-from AccessControl.SecurityManagement import noSecurityManager
-from Acquisition import aq_base
-import transaction
 import sys, code
 
 from Products.PloneTestCase import PloneTestCase
 from Products.PloneTestCase.setup import portal_name
 from Products.PloneTestCase.setup import portal_owner
+
 # setup a Plone site 
 from Products.PloneTestCase.ptc import setupPloneSite
 setupPloneSite(extension_profiles=['Archetypes:Archetypes',
-                                   'Archetypes:Archetypes_samplecontent'
+                                   'Archetypes:Archetypes_sampletypes'
                                   ])
 
 class ATSiteTestCase(PloneTestCase.PloneTestCase, attestcase.ATTestCase):
     """AT test case inside a CMF site
     """
-    
     __implements__ = PloneTestCase.PloneTestCase.__implements__ + \
                      attestcase.ATTestCase.__implements__
 
