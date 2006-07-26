7a96cfafe0a6c42d5b83d8b6790483b6d478fa95
Wichert Akkerman <wichert@wiggy.net>
2006-07-26 21:23:54 +0000

    Add a multiple catalog aware reindexObjectSecurity
    svn path=/Archetypes/branches/1.3/; revision=6719

--- Products/Archetypes/CatalogMultiplex.py
+++ Products/Archetypes/CatalogMultiplex.py
@@ -1,13 +1,12 @@
 from Globals import InitializeClass
 from Acquisition import aq_base
 from AccessControl import ClassSecurityInfo
-from Products.CMFCore.permissions import ModifyPortalContent
+from Products.CMFCore.CMFCorePermissions import ModifyPortalContent
 from Products.CMFCore.CMFCatalogAware import CMFCatalogAware
 from Products.CMFCore.utils import getToolByName
 from Products.Archetypes.Referenceable import Referenceable
 from Products.Archetypes.config import TOOL_NAME
 from Products.Archetypes.utils import shasattr
-from Products.Archetypes.config import CATALOGMAP_USES_PORTALTYPE
 
 
 class CatalogMultiplex(CMFCatalogAware):
@@ -16,39 +15,61 @@ class CatalogMultiplex(CMFCatalogAware):
     def __url(self):
         return '/'.join( self.getPhysicalPath() )
 
-    def getCatalogs(self):
-        at = getToolByName(self, TOOL_NAME, None)
-        if at is None:
-            return []
-
-        if CATALOGMAP_USES_PORTALTYPE:
-            return at.getCatalogsByType(self.portal_type)
-        else:
-            return at.getCatalogsByType(self.meta_type)
-
     security.declareProtected(ModifyPortalContent, 'indexObject')
     def indexObject(self):
-        catalogs = self.getCatalogs()
+        at = getToolByName(self, TOOL_NAME, None)
+        if at is None:
+            return
+        catalogs = at.getCatalogsByType(self.meta_type)
         url = self.__url()
         for c in catalogs:
-            # XXX This is an ugly workaround. This method shouldn't be called
-            # twice for an object in the first place, so we don't have to check
-            # if it is not cataloged so far. 
-            rid = c.getrid(url)
-            if rid is None:
-                c.catalog_object(self, url)
+            c.catalog_object(self, url)
 
     security.declareProtected(ModifyPortalContent, 'unindexObject')
     def unindexObject(self):
-        catalogs = self.getCatalogs()
+        at = getToolByName(self, TOOL_NAME)
+        catalogs = at.getCatalogsByType(self.meta_type)
         url = self.__url()
         for c in catalogs:
-            # XXX This is an ugly workaround. This method shouldn't be called
-            # twice for an object in the first place, so we don't have to check
-            # if it is still cataloged. 
-            rid = c.getrid(url)
-            if rid is not None:
-                c.uncatalog_object(url)
+            c.uncatalog_object(url)
+
+    security.declareProtected(ModifyPortalContent, 'reindexObjectSecurity')
+    def reindexObjectSecurity(self, skip_self=False):
+        """update security information in all registered catalogs.
+        """
+        at = getToolByName(self, TOOL_NAME, None)
+        if at is None:
+            return
+
+        catalogs = [c for c in at.getCatalogsByType(self.meta_type)
+                                if c is not None]
+        path = '/'.join(self.getPhysicalPath())
+
+        for catalog in catalogs:
+            for brain in catalog.unrestrictedSearchResults(path=path):
+                brain_path = brain.getPath()
+                if brain_path == path and skip_self:
+                    continue
+
+                # Get the object
+                if hasattr(aq_base(brain), '_unrestrictedGetObject'):
+                    ob = brain._unrestrictedGetObject()
+                else:
+                    # BBB: Zope 2.7
+                    ob = self.unrestrictedTraverse(brain_path, None)
+                if ob is None:
+                    # BBB: Ignore old references to deleted objects.
+                    # Can happen only in Zope 2.7, or when using
+                    # catalog-getObject-raises off in Zope 2.8
+                    LOG('reindexObjectSecurity', PROBLEM,
+                            "Cannot get %s from catalog" % brain_path)
+                    continue
+
+                # Recatalog with the same catalog uid.
+                catalog.reindexObject(ob, idxs=self._cmf_security_indexes,
+                                        update_metadata=0, uid=brain_path)
+
+
 
     security.declareProtected(ModifyPortalContent, 'reindexObject')
     def reindexObject(self, idxs=[]):
@@ -67,10 +88,11 @@ class CatalogMultiplex(CMFCatalogAware):
 
         self.http__refreshEtag()
 
-        catalogs = self.getCatalogs()
-        if not catalogs:
+        at = getToolByName(self, TOOL_NAME, None)
+        if at is None:
             return
 
+        catalogs = at.getCatalogsByType(self.meta_type)
         url = self.__url()
 
         for c in catalogs:
@@ -92,31 +114,8 @@ class CatalogMultiplex(CMFCatalogAware):
         if not idxs:
             if isinstance(self, Referenceable):
                 self._catalogUID(self)
-
-    # Hooks copied from CMFCore.CMFCatalogAware of CMF 1.6 
-    # ----------------------------------------------------
-
-    def manage_afterAdd(self, item, container):
-        """
-            Add self to the catalog.
-            (Called when the object is created or moved.)
-        """
-        self.indexObject()
-
-    def manage_afterClone(self, item):
-        """
-            Add self to the workflow.
-            (Called when the object is cloned.)
-        """
-        self.notifyWorkflowCreated()
-
-    def manage_beforeDelete(self, item, container):
-        """
-            Remove self from the catalog.
-            (Called when the object is deleted or moved.)
-        """
-        self.unindexObject()
-        #and reset the rename flag (set in Referenceable._notifyCopyOfCopyTo)
-        self._v_cp_refs = None
+                # _catalogRefs used to be called here, but all possible
+                # occurrences should be handled by
+                # manage_afterAdd/manage_beforeDelete from Referenceable now.
 
 InitializeClass(CatalogMultiplex)
