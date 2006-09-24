1ea60bea6ab8ef928adf5fb7d4d3e4904a86de29
Whit Morriss <whit@openplans.org>
2006-09-24 19:10:38 +0000

    * move validator slop out of baseobject
    * refactor tests off of layers
    
    notes: the autogenerated tests should die
    
    svn path=/Archetypes/branches/15-schema-refactoring/; revision=7021

--- Products/Archetypes/tests/atsitetestcase.py
+++ Products/Archetypes/tests/atsitetestcase.py
@@ -5,25 +5,25 @@ from Products.Archetypes.tests import attestcase
 import sys, code
 
 from Products.PloneTestCase import PloneTestCase
+from Products.PloneTestCase.layer import ZCMLLayer
 from Products.PloneTestCase.setup import portal_name
 from Products.PloneTestCase.setup import portal_owner
 
 # setup a Plone site 
-from Products.PloneTestCase.ptc import setupPloneSite
-setupPloneSite(extension_profiles=['Archetypes:Archetypes',
-                                   'Archetypes:Archetypes_sampletypes'
-                                  ])
+## from Products.PloneTestCase.ptc import setupPloneSite
+## setupPloneSite(extension_profiles=['Archetypes:Archetypes',
+##                                    'Archetypes:Archetypes_sampletypes'
+##                                   ])
 
 class ATSiteTestCase(PloneTestCase.PloneTestCase, attestcase.ATTestCase):
     """AT test case inside a CMF site
     """
-    __implements__ = PloneTestCase.PloneTestCase.__implements__ + \
-                     attestcase.ATTestCase.__implements__
-
+    layer = ZCMLLayer
+    
 class ATFunctionalSiteTestCase(Functional, ATSiteTestCase):
     """AT test case for functional tests inside a CMF site
     """
-    __implements__ = Functional.__implements__ + ATSiteTestCase.__implements__ 
+    layer = ZCMLLayer
     
     def interact(self, locals=None):
         """Provides an interactive shell aka console inside your testcase.
