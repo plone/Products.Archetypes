f89bbceb1ed231f535bd6339037d8b7ed1c9f2b7
Hanno Schlichting <hanno@hannosch.eu>
2007-01-04 10:37:42 +0000

    Backported the catalog unindexing silencer code from trunk, as in contrast to the former code on this branch it doesn't use any private attributes.
    svn path=/Archetypes/branches/1.4/; revision=7321

--- Products/Archetypes/CatalogMultiplex.py
+++ Products/Archetypes/CatalogMultiplex.py
@@ -11,6 +11,7 @@ from Products.Archetypes.config import TOOL_NAME
 from Products.Archetypes.utils import shasattr
 from Products.Archetypes.config import CATALOGMAP_USES_PORTALTYPE
 
+
 class CatalogMultiplex(CMFCatalogAware):
     security = ClassSecurityInfo()
 
@@ -39,7 +40,11 @@ class CatalogMultiplex(CMFCatalogAware):
         catalogs = self.getCatalogs()
         url = self.__url()
         for c in catalogs:
-            if c._catalog.uids.get(url, None) is not None:
+            # XXX This is an ugly workaround. This method shouldn't be called
+            # twice for an object in the first place, so we don't have to check
+            # if it is still cataloged. 
+            rid = c.getrid(url)
+            if rid is not None:
                 c.uncatalog_object(url)
 
     security.declareProtected(ModifyPortalContent, 'reindexObjectSecurity')
@@ -121,8 +126,9 @@ class CatalogMultiplex(CMFCatalogAware):
         # TODO: fix this so we can remove the following lines.
         if not idxs:
             if isinstance(self, Referenceable):
-                isCopy = getattr(self, '_v_is_cp', None)
-                if isCopy is None:
-                    self._catalogUID(self)
+                self._catalogUID(self)
+                # _catalogRefs used to be called here, but all possible
+                # occurrences should be handled by
+                # manage_afterAdd/manage_beforeDelete from Referenceable now.
 
 InitializeClass(CatalogMultiplex)
