5822a9708175a7fae1700d9eaf5f769b11fb9131
Alexander Limi <limi@plone.org>
2007-02-27 23:25:03 +0000

    Wording (don't assume that it is necessarily copyright)
    svn path=/Archetypes/trunk/; revision=7568

--- Products/Archetypes/ExtensibleMetadata.py
+++ Products/Archetypes/ExtensibleMetadata.py
@@ -151,9 +151,9 @@ class ExtensibleMetadata(Persistence.Persistent):
             'rights',
             accessor="Rights",
             widget=TextAreaWidget(
-                label=_(u'label_copyrights', default=u'Copyrights'),
+                label=_(u'label_copyrights', default=u'Rights'),
                 description=_(u'help_copyrights',
-                              default=u'The copyrights on this item.'),
+                              default=u'Copyright statement or other rights information on this item.'),
                 )),
         )) + Schema((
         # XXX change this to MetadataSchema in AT 1.4
