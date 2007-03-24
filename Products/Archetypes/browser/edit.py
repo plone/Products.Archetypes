5de469aefe6a5f0580e769da9050c78b9fbdd70e
Martin Aspeli <optilude@gmail.com>
2007-03-24 00:45:25 +0000

    FIx some spurious security declarations. Make sure the edit form doesn't show a border when used as an "add form" with portal_faactory.
    svn path=/Archetypes/trunk/; revision=7801

--- Products/Archetypes/browser/edit.py
+++ Products/Archetypes/browser/edit.py
@@ -1,5 +1,7 @@
+from zope.component import queryUtility
 from zope.interface import implements
 
+from Acquisition import aq_inner
 from Products.Five import BrowserView
 
 from Products.Archetypes.interfaces import IEditForm
@@ -7,8 +9,21 @@ from Products.Archetypes.interfaces import IMultiPageSchema
 
 from Products.Archetypes import PloneMessageFactory as _
 
+try:
+    from Products.CMFPlone.interfaces import IFactoryTool
+    HAVE_PORTAL_FACTORY = True
+except ImportError:
+    HAVE_PORTAL_FACTORY = False
+
 class Edit(BrowserView):
     implements(IEditForm)
+
+    def isTemporaryObject(self):
+        if HAVE_PORTAL_FACTORY:
+            factory = queryUtility(IFactoryTool)
+            if factory is not None:
+                return factory.isTemporary(aq_inner(self.context))
+        return False
 
     def isMultiPageSchema(self):
         return IMultiPageSchema.providedBy(self.context)
