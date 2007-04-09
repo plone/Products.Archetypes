31e61d71cdbbafd481a5db88c8e244c51e44332a
Martin Aspeli <optilude@gmail.com>
2007-04-09 16:03:27 +0000

    Make a date/time version of the field property, to convert between Zope 2 DateTime's and Python datetimes.
    svn path=/Archetypes/trunk/; revision=7884

--- Products/Archetypes/fieldproperty.py
+++ Products/Archetypes/fieldproperty.py
@@ -1,6 +1,9 @@
 """Field properties based on Archetypes schema
 """
-_marker = object()
+
+from DateTime import DateTime
+from time import mktime
+from datetime import datetime
 
 class ATFieldProperty(object):
     """Field properties based on Archetypes schema
@@ -13,6 +16,7 @@ class ATFieldProperty(object):
     sensible. However, AttributeStorage will interfere here, so we explicitly
     use annoation storage.
     
+        >>> import string
         >>> from Products.Archetypes.atapi import *
         >>> class MyContent(BaseObject):
         ...     portal_type = meta_type = 'MyContent'
@@ -23,6 +27,8 @@ class ATFieldProperty(object):
         ...
         ...     some_field = ATFieldProperty('some_field')
         ...     other_field = ATFieldProperty('_other_field')
+        ...     upper_lower = ATFieldProperty('_other_field', 
+        ...         get_transform=string.upper, set_transform=string.lower)
         
         >>> registerType(MyContent, 'Archetypes')
     
@@ -61,21 +67,86 @@ class ATFieldProperty(object):
         >>> foo.other_field
         'Good bye'
         
+    Finally, the get_transform and set_transform arguments can be used to
+    perform transformations on the retrieved value and the value before it
+    is set, respectively. The field upper_lower uses string.upper() on the
+    way out and string.lower() on the way in.
+    
+        >>> foo.upper_lower = "MiXeD"
+        >>> foo.upper_lower
+        'MIXED'
+        >>> foo.get_other_field()
+        'mixed'
+        >>> foo.set_other_field('UpPeRaNdLoWeR')
+        >>> foo.upper_lower
+        'UPPERANDLOWER'
+        
+    A less frivolous example of this functionality can be seen in the
+    ATDateTimeFieldProperty class below.
     """
 
-    def __init__(self, name):
-        self.__name = name
+    def __init__(self, name, get_transform=None, set_transform=None):
+        self._name = name
+        self._get_transform = get_transform
+        self._set_transform = set_transform
         
     def __get__(self, inst, klass):
         if inst is None:
             return self
-        field = inst.getField(self.__name)
+        field = inst.getField(self._name)
         if field is None:
-            raise KeyError("Cannot find field with name %s" % self.__name)
-        return field.get(inst)
+            raise KeyError("Cannot find field with name %s" % self._name)
+        value = field.get(inst)
+        if self._get_transform is not None:
+            value = self._get_transform(value)
+        return value
 
     def __set__(self, inst, value):
-        field = inst.getField(self.__name)
+        field = inst.getField(self._name)
         if field is None:
-            raise KeyError("Cannot find field with name %s" % self.__name)
+            raise KeyError("Cannot find field with name %s" % self._name)
+        if self._set_transform is not None:
+            value = self._set_transform(value)
         field.set(inst, value)
+        
+class ATDateTimeFieldProperty(ATFieldProperty):
+    """A field property for DateTime fields. This takes care of converting
+    to and from a Python datetime.
+    
+        >>> from Products.Archetypes.atapi import *
+        >>> class MyContent(BaseObject):
+        ...     portal_type = meta_type = 'MyContent'
+        ...     schema = Schema((
+        ...         DateTimeField('date_field', storage=AnnotationStorage()),
+        ...         ))
+        ...
+        ...     date_field = ATDateTimeFieldProperty('date_field')
+        
+        >>> registerType(MyContent, 'Archetypes')
+    
+    We can now get and set date/times.
+    
+        >>> from datetime import datetime
+        >>> target_date = datetime(2007, 4, 9, 12, 3, 12)
+        
+        >>> foo = MyContent('foo')
+        >>> foo.date_field = target_date
+        >>> foo.date_field
+        datetime.datetime(2007, 4, 9, 12, 3, 12)
+        
+        >>> foo.getDate_field().ISO()
+        '2007-04-09 12:03:12'
+        
+        >>> foo.setDate_field(DateTime('2007-04-10 13:11:01'))
+        >>> foo.date_field
+        datetime.datetime(2007, 4, 10, 13, 11, 1)
+    """
+    
+    def __init__(self, name):
+        super(ATDateTimeFieldProperty, self).__init__(name, self._zope2python_dt, self._python2zope_dt)
+        
+    def _zope2python_dt(self, zope_dt):
+        return datetime.fromtimestamp(zope_dt.timeTime())
+
+    def _python2zope_dt(self, python_dt):
+        return DateTime(mktime(python_dt.timetuple()))
