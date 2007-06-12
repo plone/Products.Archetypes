072dcb4d345573b34b8bc53451c015132a78a3b0
Maurits van Rees <maurits@vanrees.org>
2007-06-12 13:03:42 +0000

    - Add doctests for the SelectionWidget browser view. - Let that view work nicely with IntDisplayLists as well: I have a   content type with an IntegerField that uses this widget.  It would   "forget" the value for that field when you went to the edit tab.
    svn path=/Archetypes/trunk/; revision=8120

--- Products/Archetypes/tests/test_doctests.py
+++ Products/Archetypes/tests/test_doctests.py
@@ -39,6 +39,7 @@ DOCTEST_MODULES = (
     'Products.Archetypes.Field',
     'Products.Archetypes.Marshall',
     'Products.Archetypes.fieldproperty',
+    'Products.Archetypes.browser.widgets',
     )
 
 DOCTEST_FILES = ('events.txt',)
