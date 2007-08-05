6c2377bc02a2ff696375546ff7279d79faa36033
Jean-Nicolas Bes <drjnut@darksnow.org>
2007-08-05 10:48:43 +0000

    New implementation in order to avoid creating cache's dicts when it's not needed.
    svn path=/Archetypes/branches/pastis-branch/; revision=8270

--- Products/Archetypes/cache.py
+++ Products/Archetypes/cache.py
@@ -4,27 +4,45 @@ from persistent.dict import PersistentDict
 
 from Products.Archetypes import interfaces
 
-@interface.implementer(interfaces.ITransformCache)
-@component.adapter(interfaces.IField, interfaces.IBaseObject)
-def transform_cache(field, instance):
-    """
-    Test if we get a PersistentDict by adapting (field, obj).
+class TransformCache:
+    interface.implements(interfaces.ITransformCache)
     
-    >>> from Products.Archetypes.examples.SimpleFile import SimpleFile
-    >>> obj = SimpleFile('myFile')
-    >>> transforms_cache = component.getMultiAdapter((obj.getField('body'), obj), interfaces.ITransformCache)
-    >>> transforms_cache
-    <persistent.dict.PersistentDict object at ...>
+    def __init__(self, field, instance):
+        self.field = field
+        self.instance = instance
     
-    Test if we get the same value that we put in.
+    def getCacheIfExists(self):
+        ann = IAnnotations(self.instance)
+        transforms_cache = ann.get('Products.Archetypes:transforms-cache', None)
+        if transforms_cache is None:
+            return
+        field_cache = transforms_cache.get(self.field.getName(), None)
+        if field_cache is None:
+            return
+
+    def getOrCreateCache(self):
+        ann = IAnnotations(self.instance)
+        transforms_cache = ann.setdefault(
+            'Products.Archetypes:transforms-cache', PersistentDict())
+        field_cache = transforms_cache.setdefault(self.field.getName(),
+                                                  PersistentDict())
+        return field_cache
+
+    def clear(self):
+        field_cache = self.getCacheIfExists()
+        if field_cache is None:
+            return
+        field_cache.clear()
     
-    >>> transforms_cache['text/plain'] = 'The text/plain representation of the FileField'
-    >>> transforms_cache = component.getMultiAdapter((obj.getField('body'), obj), interfaces.ITransformCache)
-    >>> transforms_cache['text/plain']
-    'The text/plain representation of the FileField'
-    """
-    ann = IAnnotations(instance)
-    transforms_cache = ann.setdefault(
-        'Products.Archetypes:transforms-cache', PersistentDict())
-    field_cache = transforms_cache.setdefault(field.getName(), PersistentDict())
-    return field_cache
+    def __getitem__(self, key):
+        field_cache = self.getCacheIfExists()
+        if field_cache is None:
+            return None
+        return field_cache[key]
+    
+    def __setitem__(self, key, value):
+        if value is None:
+            return
+        field_cache = self.getOrCreateCache()
+        field_cache[key] = value
+
