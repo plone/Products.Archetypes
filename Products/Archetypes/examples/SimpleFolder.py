e70a397d261d53dbab0b94caf8ccc7890022619a
Wichert Akkerman <wichert@wiggy.net>
2007-08-11 22:45:11 +0000

    Remove accidental change
    svn path=/Archetypes/trunk/; revision=8335

--- Products/Archetypes/examples/SimpleFolder.py
+++ Products/Archetypes/examples/SimpleFolder.py
@@ -6,7 +6,6 @@ schema = BaseSchema
 class SimpleFolder(BaseFolder):
     """A simple folderish archetype"""
     schema = schema
-    portal_tye = "SimpleFolder"
 
     def manage_afterMKCOL(self, id, result, REQUEST=None, RESPONSE=None):
         """For unit tests
