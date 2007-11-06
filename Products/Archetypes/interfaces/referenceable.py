406bee4b68251f0950b9421275805f0ef63de0ac
Daniel Nouri <daniel.nouri@gmail.com>
2007-11-06 23:57:06 +0000

    Retag
    svn path=/Archetypes/tags/1.5.4/; revision=8873

--- Products/Archetypes/interfaces/referenceable.py
+++
@@ -1,7 +0,0 @@
-"""
-Referenceable interface
-"""
-
-# BBB: module will be removed
-#      zope2 interfaces created at runtime
-#      - IReferenceable
--- Products/Archetypes/public.py
+++
@@ -1,5 +0,0 @@
-"""
-backward compatibility module, don't remove it as too many products rely on it.
-"""
-# all current from atapi
-from Products.Archetypes.atapi import *
