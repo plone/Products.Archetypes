958a67a396286d38936e39b07a55666c6f181147
Wichert Akkerman <wichert@wiggy.net>
2008-01-01 20:24:56 +0000

    Archetypes is an egg now
    svn path=/Archetypes/trunk/; revision=9184

--- Products/Archetypes/interfaces/_metadata.py
+++
@@ -1,7 +0,0 @@
-from Products.CMFCore.interfaces import ICatalogableDublinCore
-from Products.CMFCore.interfaces import IMutableDublinCore
-
-class IExtensibleMetadata(ICatalogableDublinCore,IMutableDublinCore):
-    """ Archetypes implementation of DublinCore metadata """
-
-
