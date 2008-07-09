c9ddaf63209a2c86be97ef82137c3e7bebe9c384
Dorneles Tremea <dorneles@x3ng.com.br>
2008-07-09 04:22:25 +0000

    Fixed CatalogMultiplex.reindexObjectSecurity to check for the specific CMF catalog interface before trying to update the security stuff. This closes #8271.
    svn path=/Products.Archetypes/trunk/; revision=9841

---
+++ Products/Archetypes/CatalogMultiplex.py
@@ -0,0 +1,129 @@
+from debug import log
+from logging import WARNING
+
+from Acquisition import aq_base
+from AccessControl import ClassSecurityInfo
+from Globals import InitializeClass
+from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.interfaces import ICatalogTool
+from Products.CMFCore.permissions import ModifyPortalContent
+from Products.CMFCore.CMFCatalogAware import CMFCatalogAware
+from Products.Archetypes.config import CATALOGMAP_USES_PORTALTYPE, TOOL_NAME
+from Products.Archetypes.Referenceable import Referenceable
+from Products.Archetypes.utils import shasattr
+
+class CatalogMultiplex(CMFCatalogAware):
+    security = ClassSecurityInfo()
+
+    def __url(self):
+        return '/'.join( self.getPhysicalPath() )
+
+    def getCatalogs(self):
+        at = getToolByName(self, TOOL_NAME, None)
+        if at is None:
+            return []
+
+        if CATALOGMAP_USES_PORTALTYPE:
+            return at.getCatalogsByType(self.portal_type)
+        else:
+            return at.getCatalogsByType(self.meta_type)
+
+    security.declareProtected(ModifyPortalContent, 'indexObject')
+    def indexObject(self):
+        catalogs = self.getCatalogs()
+        url = self.__url()
+        for c in catalogs:
+            c.catalog_object(self, url)
+
+    security.declareProtected(ModifyPortalContent, 'unindexObject')
+    def unindexObject(self):
+        catalogs = self.getCatalogs()
+        url = self.__url()
+        for c in catalogs:
+            if c._catalog.uids.get(url, None) is not None:
+                c.uncatalog_object(url)
+
+    security.declareProtected(ModifyPortalContent, 'reindexObjectSecurity')
+    def reindexObjectSecurity(self, skip_self=False):
+        """update security information in all registered catalogs.
+        """
+        at = getToolByName(self, TOOL_NAME, None)
+        if at is None:
+            return
+
+        catalogs = [c for c in at.getCatalogsByType(self.meta_type)
+                               if ICatalogTool.providedBy(c)]
+        path = '/'.join(self.getPhysicalPath())
+
+        for catalog in catalogs:
+            for brain in catalog.unrestrictedSearchResults(path=path):
+                brain_path = brain.getPath()
+                if brain_path == path and skip_self:
+                    continue
+
+                # Get the object
+                if hasattr(aq_base(brain), '_unrestrictedGetObject'):
+                    ob = brain._unrestrictedGetObject()
+                else:
+                    # BBB: Zope 2.7
+                    ob = self.unrestrictedTraverse(brain_path, None)
+                if ob is None:
+                    # BBB: Ignore old references to deleted objects.
+                    # Can happen only in Zope 2.7, or when using
+                    # catalog-getObject-raises off in Zope 2.8
+                    log("reindexObjectSecurity: Cannot get %s from catalog" %
+                        brain_path, level=WARNING)
+                    continue
+
+                # Recatalog with the same catalog uid.
+                catalog.reindexObject(ob, idxs=self._cmf_security_indexes,
+                                        update_metadata=0, uid=brain_path)
+
+
+
+    security.declareProtected(ModifyPortalContent, 'reindexObject')
+    def reindexObject(self, idxs=[]):
+        """update indexes of this object in all registered catalogs.
+
+        Catalogs are registered per 'meta_type' in archetypes tool.
+
+        'idxs' are a list of index names. If this list is given only the given
+        indexes are refreshed. If a index does not exist in catalog its
+        silently ignored.
+        """
+        if idxs == [] and shasattr(self, 'notifyModified'):
+            # Archetypes default setup has this defined in ExtensibleMetadata
+            # mixin. note: this refreshes the 'etag ' too.
+            self.notifyModified()
+
+        self.http__refreshEtag()
+
+        catalogs = self.getCatalogs()
+        if not catalogs:
+            return
+
+        url = self.__url()
+
+        for c in catalogs:
+            if c is not None:
+                # We want the intersection of the catalogs idxs
+                # and the incoming list.
+                lst = idxs
+                indexes = c.indexes()
+                if idxs:
+                    lst = [i for i in idxs if i in indexes]
+                c.catalog_object(self, url, idxs=lst)
+
+        # We only make this call if idxs is not passed.
+        #
+        # manage_afterAdd/manage_beforeDelete from Referenceable take
+        # care of most of the issues, but some places still expect to
+        # call reindexObject and have the uid_catalog updated.
+        # TODO: fix this so we can remove the following lines.
+        if not idxs:
+            if isinstance(self, Referenceable):
+                isCopy = getattr(self, '_v_is_cp', None)
+                if isCopy is None:
+                    self._catalogUID(self)
+
+InitializeClass(CatalogMultiplex)
---
+++ Products/Archetypes/tests/test_index.py
@@ -0,0 +1,158 @@
+################################################################################
+#
+# Copyright (c) 2002-2005, Benjamin Saller <bcsaller@ideasuite.com>, and
+#                              the respective authors. All rights reserved.
+# For a list of Archetypes contributors see docs/CREDITS.txt.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of the author nor the names of its contributors may be used
+#   to endorse or promote products derived from this software without specific
+#   prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+################################################################################
+"""
+"""
+
+import time
+from Testing import ZopeTestCase
+from Products.Archetypes.atapi import *
+from Products.Archetypes.tests.atsitetestcase import ATSiteTestCase
+from Products.CMFCore.utils import getToolByName
+from Products.Archetypes.tests.utils import makeContent
+from Products.ZCatalog.ZCatalog import manage_addZCatalog
+
+class ETagTest(ATSiteTestCase):
+
+    def afterSetUp(self):
+        self.setRoles(['Manager'])
+        self.inst = makeContent(self.portal,
+                                portal_type='SimpleType',
+                                id='simple_type')
+
+    #! time.sleep(1) is needed to get tests running on fast machines
+
+    def test_etag_does_update_reindex_all(self):
+        before = self.inst.http__etag(readonly=True)
+        time.sleep(1)
+        self.inst.reindexObject()
+        after = self.inst.http__etag(readonly=True)
+        self.failIf(before == after)
+
+    def test_etag_update_on_edit(self):
+        before = self.inst.http__etag(readonly=True)
+        time.sleep(1)
+        self.inst.edit(title='Bla')
+        after = self.inst.http__etag(readonly=True)
+        self.failIf(before == after)
+
+    def test_etag_update_on_update(self):
+        before = self.inst.http__etag(readonly=True)
+        time.sleep(1)
+        self.inst.update(title='Bla')
+        after = self.inst.http__etag(readonly=True)
+        self.failIf(before == after)
+
+    def test_etag_update_on_processform(self):
+        before = self.inst.http__etag(readonly=True)
+        time.sleep(1)
+        self.inst.processForm(data=1, values={'title':'Bla'})
+        after = self.inst.http__etag(readonly=True)
+        self.failIf(before == after)
+
+
+class ReindexTest(ATSiteTestCase):
+
+    def afterSetUp(self):
+        self.setRoles(['Manager'])
+        self.inst = makeContent(self.portal,
+                                portal_type='SimpleType',
+                                id='simple_type')
+        self.ct = getToolByName(self.portal, 'portal_catalog')
+
+    def test_reindex_unindexes_old(self):
+        ct = self.ct
+        self.assertEquals(len(ct(SearchableText='Mosquito')), 0)
+        self.inst.edit(title='Mosquito')
+        self.assertEquals(len(ct(SearchableText='Mosquito')), 1)
+        self.inst.edit(title='Libido')
+        self.assertEquals(len(ct(SearchableText='Mosquito')), 0)
+
+
+class MultiplexTest(ATSiteTestCase):
+
+    def afterSetUp(self):
+        self.setRoles(['Manager'])
+        manage_addZCatalog(self.portal, 'zope_catalog', 'Zope Catalog')
+        self.portal.zope_catalog.addIndex('getId', 'FieldIndex')
+        self.tool = self.portal.archetype_tool
+        self.pc = getToolByName(self.portal, 'portal_catalog')
+        self.zc = getToolByName(self.portal, 'zope_catalog')
+
+    def test_default_catalog(self):
+        inst = makeContent(self.portal,
+                           portal_type='SimpleType',
+                           id='simple_type')
+
+        # Make sure the object is indexed by portal_catalog...
+        results = self.pc.searchResults(getId='simple_type')
+        self.failUnlessEqual(len(results), 1)
+        self.failUnlessEqual(results[0].getObject(), inst)
+
+        # ...but isn't by the zope_catalog
+        results = self.zc.searchResults(getId='simple_type')
+        self.failUnlessEqual(len(results), 0)
+
+    def test_new_catalog(self):
+        # Change SimpleType to use only zope_catalog
+        self.tool.setCatalogsByType('SimpleType', ['zope_catalog'])
+        inst = makeContent(self.portal,
+                           portal_type='SimpleType',
+                           id='simple_type')
+
+        # Make sure the object is indexed by the new zope_catalog...
+        results = self.zc.searchResults(getId='simple_type')
+        self.failUnlessEqual(len(results), 1)
+        self.failUnlessEqual(results[0].getObject(), inst)
+
+        # ...but isn't indexed anymore by portal_catalog
+        results = self.pc.searchResults(getId='simple_type')
+        self.failUnlessEqual(len(results), 0)
+
+    def test_both_catalogs(self):
+        # Change SimpleType to use both catalogs
+        self.tool.setCatalogsByType('SimpleType',
+                                    ['portal_catalog', 'zope_catalog'])
+        inst = makeContent(self.portal,
+                           portal_type='SimpleType',
+                           id='simple_type')
+
+        # Make sure the object is indexed by portal_catalog...
+        results = self.pc.searchResults(getId='simple_type')
+        self.failUnlessEqual(len(results), 1)
+        self.failUnlessEqual(results[0].getObject(), inst)
+
+        # ...and also by the zope_catalog
+        results = self.zc.searchResults(getId='simple_type')
+        self.failUnlessEqual(len(results), 1)
+        self.failUnlessEqual(results[0].getObject(), inst)
+
+
+def test_suite():
+    from unittest import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(ETagTest))
+    suite.addTest(makeSuite(ReindexTest))
+    suite.addTest(makeSuite(MultiplexTest))
+    return suite
