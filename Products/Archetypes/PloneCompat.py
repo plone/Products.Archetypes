67607daf47d760a014496e8fb8722c4d6413567b
Hanno Schlichting <hanno@hannosch.eu>
2008-11-01 12:38:54 +0000

    Removed tabindex and Iterator from the template scopes. Their use was discouraged long ago.
    svn path=/Products.Archetypes/trunk/; revision=10203

--- Products/Archetypes/PloneCompat.py
+++ Products/Archetypes/PloneCompat.py
@@ -6,22 +6,6 @@ import logging
 logger = logging.getLogger('Archetypes')
 
 try:
-    from Products.CMFPlone.utils import IndexIterator
-except ImportError:
-    class IndexIterator:
-        __allow_access_to_unprotected_subobjects__ = 1
-
-        def __init__(self, upper=100000, pos=0):
-            self.upper=upper
-            self.pos=pos
-
-        def next(self):
-            if self.pos <= self.upper:
-                self.pos += 1
-                return self.pos
-            raise KeyError, 'Reached upper bounds'
-
-try:
     from Products.CMFPlone.utils import transaction_note
 except ImportError:
     def transaction_note(note):
