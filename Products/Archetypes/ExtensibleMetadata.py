337d28dd77dae67f11698c7e166e20ee3f93462e
David Glick <davidglick@groundwire.org>
2009-09-21 21:15:08 +0000

    merge r11011 from AT trunk: Categories are now Tags
    svn path=/Products.Archetypes/branches/1.6/; revision=11368

--- Products/Archetypes/ExtensibleMetadata.py
+++ Products/Archetypes/ExtensibleMetadata.py
@@ -76,10 +76,10 @@ class ExtensibleMetadata(Persistence.Persistent):
             accessor="Subject",
             searchable=True,
             widget=KeywordWidget(
-                label=_(u'label_categories', default=u'Categories'),
-                description=_(u'help_categories',
-                              default=u'Also known as keywords, tags or labels, '
-                                       'these help you categorize your content.'),
+                label=_(u'label_tags', default=u'Tags'),
+                description=_(u'help_tags',
+                              default=u'Tags are commonly used for ad-hoc '
+                                       'organization of content.'),
                 ),
         ),
         TextField(
