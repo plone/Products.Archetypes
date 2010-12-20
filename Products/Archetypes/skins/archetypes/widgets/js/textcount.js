5af72f59daff833434c035bca51f2483093fa534
Tom Gross <itconsense@gmail.com>
2010-12-20 13:20:07 +0000

    Fixed textCounter JavaScript to work with fieldnames with hyphen. https://dev.plone.org/plone/ticket/11334
    svn path=/Products.Archetypes/trunk/; revision=13374

--- Products/Archetypes/skins/archetypes/widgets/js/textcount.js
+++ Products/Archetypes/skins/archetypes/widgets/js/textcount.js
@@ -1,19 +1,13 @@
 <!-- Original:  Ronnie T. Moore -->
 <!-- Dynamic 'fix' by: Nannette Thacker -->
-
 function textCounter(field, countfield, maxlimit) {
-
-	if (field.value.length > maxlimit) {
-	
-		// if too long...trim it!
-		field.value = field.value.substring(0, maxlimit);
-		
-        alert( 'This field is limited to ' + maxlimit + ' characters in length.' );
-        		
-	} else {
-	
-		// otherwise, update 'characters left' counter	
-		countfield.value = maxlimit - field.value.length;
-		
-	}
+  var fieldval = jq(field).attr('value');
+  if (fieldval.length > maxlimit) {
+      // if too long...trim it!
+      jq(field).attr('value',  fieldval.substring(0, maxlimit));
+      alert( 'This field is limited to ' + maxlimit + ' characters in length.' );
+  } else {
+      // otherwise, update 'characters left' counter	
+      jq('input[name="' + countfield + '"]').attr('value', maxlimit - fieldval.length);
+  }
 }
