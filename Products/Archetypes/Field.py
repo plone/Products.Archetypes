cd20375782e4f4f9ddb5237f0746e9cae0d696d4
Franco Pellegrini <frapell@gmail.com>
2013-07-29 18:10:46 -0300

    Add a transaction note when setting a default value for a field which doesn't have a storage yet

--- Products/Archetypes/Field.py
+++ Products/Archetypes/Field.py
@@ -1,3 +1,5 @@
+import transaction
+
 from copy import deepcopy
 from cgi import escape
 from cStringIO import StringIO
@@ -714,6 +716,10 @@ class ObjectField(Field):
             # @@ and at every other possible occurence of an AttributeError?!!
             default = self.getDefault(instance)
             if not kwargs.get('_initializing_', False):
+                transaction.get().note("'%s' field missing value on instance "
+                                       "'%s'; setting default value." %
+                                       (self.getName(),
+                                        '/'.join(instance.getPhysicalPath())))
                 self.set(instance, default, _initializing_=True, **kwargs)
             return default
 
@@ -1391,6 +1397,10 @@ class TextField(FileField):
         except AttributeError:
             # happens if new Atts are added and not yet stored in the instance
             if not kwargs.get('_initializing_', False):
+                transaction.get().note("'%s' field missing value on instance "
+                                       "'%s'; setting default value." %
+                                       (self.getName(),
+                                        '/'.join(instance.getPhysicalPath())))
                 self.set(instance, self.getDefault(instance),
                          _initializing_=True, **kwargs)
             value = self._wrapValue(instance, self.getDefault(instance))
