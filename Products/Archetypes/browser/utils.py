68b42de6eb98cec557fad65d83043f58082c2aa1
Maurits van Rees <maurits@vanrees.org>
2013-08-28 23:05:49 +0200

    Fix translating integer display lists.
    Refs pull request #25.

--- Products/Archetypes/browser/utils.py
+++ Products/Archetypes/browser/utils.py
@@ -2,7 +2,6 @@ from zope.interface import implements
 from Products.Five import BrowserView
 from Products.Archetypes.interfaces.utils import IUtils
 from zope.i18n import translate
-from zope.i18nmessageid import Message
 
 
 class Utils(BrowserView):
