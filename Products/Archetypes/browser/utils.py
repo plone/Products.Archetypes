4bee3f491d03054349fc62d061b380902eee83bd
Maurits van Rees <maurits@vanrees.org>
2013-08-28 23:05:49 +0200

    Fix translating integer display lists.
    Refs pull request #25.

--- Products/Archetypes/browser/utils.py
+++ Products/Archetypes/browser/utils.py
@@ -2,16 +2,35 @@ from zope.interface import implements
 from Products.Five import BrowserView
 from Products.Archetypes.interfaces.utils import IUtils
 from zope.i18n import translate
-from zope.i18nmessageid import Message
 
 
 class Utils(BrowserView):
     implements(IUtils)
 
     def translate(self, vocab, value, widget=None):
+        """Translate an input value from a vocabulary.
+
+        - vocab is a vocabulary, for example a DisplayList or IntDisplayList
+
+        - 'value' is meant as 'input value' and should have been
+          called 'key', really, because we will lookup this key in the
+          vocabulary, which should give us a value as answer.  When no
+          such value is known, we take the original input value.  This
+          gets translated.
+
+        - By passing a widget with a i18n_domain attribute, we use
+          that as the translation domain.  The default is 'plone'.
+
+        Supported input values are at least: string, integer, list and
+        tuple.  When there are multiple values, we iterate over them.
+        """
         domain = 'plone'
         context = self.context
-        if isinstance(value, basestring):
+        # Make sure value is an iterable.  There are really too many
+        # iterable and non-iterable types (and half-iterable like
+        # strings, which we definitely do not want to iterate over) so
+        # we check the __iter__ attribute:
+        if not hasattr(value, '__iter__'):
             value = [value]
         if widget:
             custom_domain = getattr(widget, 'i18n_domain', None)
@@ -28,11 +47,14 @@ class Utils(BrowserView):
                     continue
                 v_encoded = context.unicodeEncode(v)
                 vocab_value = vocab.getValue(v_encoded, v)
-                # avoid UnicodeDecodeError if value contains special chars
-                if not isinstance(vocab_value, unicode):
+                if not isinstance(vocab_value, basestring):
+                    # May be integer.
+                    vocab_value = str(vocab_value)
+                elif not isinstance(vocab_value, unicode):
+                    # avoid UnicodeDecodeError if value contains special chars
                     vocab_value = unicode(vocab_value, 'utf-8')
                 # translate explicitly
                 vocab_value = _(vocab_value)
                 nvalues.append(vocab_value)
-            value =  ', '.join(nvalues)
+            value = ', '.join(nvalues)
         return value
