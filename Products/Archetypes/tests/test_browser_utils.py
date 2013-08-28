19992753654259f7da49a64d1ac0c2625db25f01
Maurits van Rees <maurits@vanrees.org>
2013-08-28 23:28:22 +0200

    Test that translating unicode values from vocabs goes right.
    No longer use the unicodeEncode python skin script here, as the part we need is really simple.

--- Products/Archetypes/tests/test_browser_utils.py
+++ Products/Archetypes/tests/test_browser_utils.py
@@ -38,18 +38,6 @@ class UtilsMethodsTests(ATSiteTestCase):
         self.assertEqual(utilsView.translate(vocab, value=3),
                          u'[[plone][42]]')
 
-    def test_translate_integer_display_list(self):
-        vocab = IntDisplayList(((1, 'Sp\xc3\xa9cial char value'),
-                                (2, 'two'),
-                                (3, 42), ))
-        utilsView = getMultiAdapter((self.portal, self.portal.REQUEST), name='at_utils')
-        self.assertEqual(utilsView.translate(vocab, value=1),
-                         u'[[plone][Sp\xe9cial char value]]')
-        self.assertEqual(utilsView.translate(vocab, value=2),
-                         u'[[plone][two]]')
-        self.assertEqual(utilsView.translate(vocab, value=3),
-                         u'[[plone][42]]')
-
 
 def test_suite():
     suite = TestSuite()
