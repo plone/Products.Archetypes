cbccd42367a5456e2f1e1bfe72933f9574edbdb5
Yulia Kallistratova <kallistratova@gmail.com>
2014-05-22 15:14:41 +0200

    completes https://dev.plone.org/ticket/9896
    Added updating CreationDate index so it shows correctly in folder_contents and byliner

--- Products/Archetypes/subscribers.py
+++ Products/Archetypes/subscribers.py
@@ -4,3 +4,4 @@ from DateTime import DateTime
 def updateCreationOnCopy(obj, event):
     """ Update creation date when object duplicated via copy/paste"""
     obj.setCreationDate(DateTime())
+    obj.reindexObject('CreationDate')
