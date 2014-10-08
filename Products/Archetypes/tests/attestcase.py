77a9000bb5b1d12b171c6689772336b213e840b3
Tom Gross <itconsense@gmail.com>
2014-10-08 11:15:07 +0200

    first cut in moving to p.a.testing

--- Products/Archetypes/tests/attestcase.py
+++ Products/Archetypes/tests/attestcase.py
@@ -1,19 +1,7 @@
-from Testing import ZopeTestCase
-from Testing.ZopeTestCase import user_name
-from Testing.ZopeTestCase import user_password
-from Testing.ZopeTestCase.functional import Functional
-
-from Products.CMFTestCase import CMFTestCase
-from Products.CMFTestCase.ctc import setupCMFSite
-from Products.CMFTestCase.layer import onsetup
+from plone.app.testing.bbb import PloneTestCase
 from Products.GenericSetup import EXTENSION, profile_registry
 
-from Products.Archetypes.tests.layer import ZCML
 
-default_user = user_name
-default_role = 'Member'
-
-@onsetup
 def setupSampleTypeProfile():
     profile_registry.registerProfile('Archetypes_sampletypes',
         'Archetypes Sample Content Types',
@@ -22,30 +10,10 @@ def setupSampleTypeProfile():
         'Products.Archetypes',
         EXTENSION)
 
-# setup a CMF site
-ZopeTestCase.installProduct('PythonScripts')
-ZopeTestCase.installProduct('SiteErrorLog')
-ZopeTestCase.installProduct('CMFFormController')
-ZopeTestCase.installProduct('CMFQuickInstallerTool')
-ZopeTestCase.installProduct('MimetypesRegistry')
-ZopeTestCase.installProduct('PortalTransforms')
-ZopeTestCase.installProduct('Archetypes')
-
 setupSampleTypeProfile()
-setupCMFSite(
-    extension_profiles=['Products.CMFFormController:CMFFormController',
-                        'Products.CMFQuickInstallerTool:CMFQuickInstallerTool',
-                        'Products.MimetypesRegistry:MimetypesRegistry',
-                        'Products.PortalTransforms:PortalTransforms',
-                        'Products.Archetypes:Archetypes',
-                        'Products.Archetypes:Archetypes_sampletypes'])
 
-class ATTestCase(ZopeTestCase.ZopeTestCase):
+class ATTestCase(PloneTestCase):
     """Simple AT test case
     """
-    layer = ZCML
 
-class ATFunctionalTestCase(Functional, ATTestCase):
-    """Simple AT test case for functional tests
-    """
-    layer = ZCML
+ATFunctionalTestCase = ATTestCase
--- Products/Archetypes/tests/layer.py
+++ Products/Archetypes/tests/layer.py
@@ -1,57 +1,26 @@
-from Testing.ZopeTestCase.layer import ZopeLite
-
 from zope.testing.cleanup import cleanUp
-
-from Products.CMFCore.testing import FunctionalZCMLLayer
-from Products.CMFTestCase import layer as cmf_layer
-
-# BBB Zope 2.12
-try:
-    from Zope2.App import zcml
-except ImportError:
-    from Products.Five import zcml
+from plone.app.testing import PloneSandboxLayer
 
 
-class ZCML(FunctionalZCMLLayer):
+class ArchettypesFixture(PloneSandboxLayer):
 
-    def setUp(cls):
+    def setUpZope(self, app, configurationContext):
         '''Sets up the CA.'''
-        import Products.CMFDefault
-        zcml.load_config('configure.zcml', Products.CMFDefault)
-
         import Products.DCWorkflow
-        zcml.load_config('configure.zcml', Products.DCWorkflow)
+        self.loadZCML(package=Products.DCWorkflow)
 
         import Products.Archetypes
-        zcml.load_config('configure.zcml', Products.Archetypes)
+        self.loadZCML(package=Products.Archetypes)
 
         import Products.PlacelessTranslationService
-        zcml.load_config('configure.zcml', Products.PlacelessTranslationService)
+        self.loadZCML(package=Products.PlacelessTranslationService)
 
         import plone.uuid
-        zcml.load_config('configure.zcml', plone.uuid)
+        self.loadZCML(package=plone.uuid)
 
-    setUp = classmethod(setUp)
-
-    def tearDown(cls):
+    def tearDownZope(cls):
         '''Cleans up the CA.'''
+        # XXX needed?
         cleanUp()
-    tearDown = classmethod(tearDown)
 
-
-class ArchetypesSite(ZCML):
-
-    def setUp(cls):
-        '''Sets up the CA.'''
-        for func, args, kw in cmf_layer._deferred_setup:
-            func(*args, **kw)
-
-    setUp = classmethod(setUp)
-
-    def tearDown(cls):
-        '''Cleans up the CA.'''
-        for func, args, kw in cmf_layer._deferred_cleanup:
-            func(*args, **kw)
-
-        # cleanUp()
-    tearDown = classmethod(tearDown)
+AT_FIXTURE = ArchettypesFixture()
