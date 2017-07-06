7475150e2c75766199c30af83c71a6c02efe6b76
Valtteri Karppinen <karppinenvaltteri@gmail.com>
2017-07-06 10:33:55 +0300

    Fixed comments and clarified variable names

--- Products/Archetypes/skins/archetypes/widgets/js/textcount.js
+++ Products/Archetypes/skins/archetypes/widgets/js/textcount.js
@@ -1,13 +1,15 @@
-<!-- Original:  Ronnie T. Moore -->
-<!-- Dynamic 'fix' by: Nannette Thacker -->
+// Original:  Ronnie T. Moore
+// Dynamic 'fix' by: Nannette Thacker
 function textCounter(field, countfield, maxlimit) {
   var fieldval = jQuery(field).val();
-  var countfield = jQuery('input[name="' + countfield + '"]');
+  var counterElem = jQuery('input[name="' + countfield + '"]');
   if (fieldval.length > maxlimit) {
-      // if too long...trim it!
-      jQuery(field).val(fieldval.substring(0, maxlimit));
-      countfield.css('border', '1px solid red');
+    // if too long...trim it!
+    jQuery(field).val(fieldval.substring(0, maxlimit));
+    counterElem.css('border-color', 'red');
+  } else {
+    counterElem.css('border-color', '#ccc');
   }
   // update 'characters left' counter
-  countfield.attr('value', Math.max(maxlimit - fieldval.length, 0));
+  counterElem.val(Math.max(maxlimit - fieldval.length, 0));
 }
